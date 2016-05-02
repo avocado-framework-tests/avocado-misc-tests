75894ce1120120c3b834d5b93660f65c69fa0d10
Harish <harisrir@linux.vnet.ibm.com>
2016-05-02 13:26:19 +0530

    Handling dependencies for Dbench test

--- io/disk/dbench.py
+++ io/disk/dbench.py
@@ -8,6 +8,7 @@ from avocado import main
 from avocado.utils import archive
 from avocado.utils import process
 from avocado.utils import build
+from avocado.utils.software_manager import SoftwareManager
 
 
 class Dbench(Test):
@@ -25,12 +26,9 @@ class Dbench(Test):
         Source:
         http://samba.org/ftp/tridge/dbench/dbench-3.04.tar.gz
         '''
-
-        try:
-            process.run('which gcc')
-        except:
-            self.error('gcc is required by this job and is not available on'
-                       'the system')
+        sm = SoftwareManager()
+        if not sm.check_installed("gcc") and not sm.install("gcc"):
+            self.error('Gcc is needed for the test to be run')
 
         self.results = []
         data_dir = os.path.abspath(self.datadir)
