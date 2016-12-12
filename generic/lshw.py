3979d9a08217825e2d6fb604b6cad6f09e8ce0fe
Ramya BS <ramya@linux.vnet.ibm.com>
2016-12-12 15:59:56 +0530

    Test case for lshw tool (Hardware lister)
    Description:
    lshw is a small tool to provide detailed informaton on the hardware
    configuration of the machine. It can report exact memory
    configuration, firmware version, mainboard configuration, CPU version
    and speed, cache configuration, bus speed, etc. on DMI-capable x86
    systems and on some PowerPC machines (PowerMac G4 is known to work).
    
    Signed-off-by: Ramya BS ramya@linux.vnet.ibm.com
    Tested-by : Ramya BS <ramya@linux.vnet.ibm.com

---
+++ generic/lshw.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: 2016 IBM.
+# Author: Ramya BS <ramya@linux.vnet.ibm.com>
+
+import re
+from avocado import Test
+from avocado import main
+from avocado.utils import process
+from avocado.utils.software_manager import SoftwareManager
+
+
+class Lshwrun(Test):
+
+    """
+    lshw is a small tool to extract detailed information on the
+    hardware configuration of the machine.
+    It can report exact memory configuration,firmware version,
+    mainboard configuration, CPU version and speed,cache configuration, bus
+    speed, etc. on DMI-capable x86 or IA-64 systems and on some PowerPC
+    machines (PowerMac G4 is known to work).
+    """
+    interface = process.system_output("ip route show")
+    active_interface = re.search(
+        r"default via\s+\S+\s+dev\s+(\w+)\s+proto", interface).group(1)
+    fail_cmd = list()
+
+    def run_cmd(self, cmd):
+        if process.system(cmd, ignore_status=True, sudo=True, shell=True):
+            self.is_fail += 1
+            self.fail_cmd.append(cmd)
+
+    def setUp(self):
+        sm = SoftwareManager()
+        for package in ("lshw", "net-tools", "iproute", "pciutils"):
+            if not sm.check_installed(package) and not sm.install(package):
+                self.error("Fail to install %s required for this"
+                           " test." % package)
+
+    def test_lshw(self):
+        """
+        lshw without any options would generate full information
+        report about all detected hardware.
+        """
+        self.log.info("===============Executing lshw test ===="
+                      "===========")
+        self.is_fail = 0
+        self.run_cmd("lshw")
+        self.run_cmd("lshw -version")
+        if self.is_fail >= 1:
+            self.fail("%s command(s) failed to execute  "
+                      % self.fail_cmd)
+
+    def test_lshw_short(self):
+        """
+        With "-short" option,the lshw command would generate a brief
+        information report about the hardware devices
+        """
+        self.log.info("===============Executing lshw -short tests ===="
+                      "===========")
+        self.is_fail = 0
+        for class_name in ['network', 'storage', 'memory', 'power',
+                           'bus', 'processor', 'system']:
+            self.run_cmd("lshw -short -class  %s" % class_name)
+        if self.is_fail >= 1:
+            self.fail("%s command(s) failed to execute "
+                      % self.fail_cmd)
+
+    def test_lshw_class(self):
+        """
+        To display information about any particular hardware,specify the class.
+        """
+        self.log.info("===============Executing lshw -class tests ===="
+                      "===========")
+        self.is_fail = 0
+        for hw_class in ['disk', 'storage', 'memory', 'cpu', 'volume',
+                         'network', 'power', 'generic', 'processor',
+                         'bridge', 'multimedia', 'display',
+                         'system', 'communication', 'bus']:
+            self.run_cmd("lshw -class %s" % hw_class)
+        if self.is_fail >= 1:
+            self.fail("%s command(s) failed to execute "
+                      % self.fail_cmd)
+
+    def test_lshw_verification(self):
+        """
+        compare the output of lshw with other tools
+        which produces similar info of hardware.
+        """
+        # verifying mac address
+        get_mac = process.system_output(
+            " ip link show %s " % self.active_interface)
+        mac = re.search(r'link\/\ether (.*) brd', get_mac).group(1)
+        if mac not in process.system_output("lshw"):
+            self.fail("lshw failed to show correct mac address")
+
+        # verify network
+        if self.active_interface not in process.system_output(
+                "lshw -class network"):
+            self.fail("lshw failed to show correct active network interface")
+
+    def test_gen_rep(self):
+        """
+        Lshw is capable of producing reports in html, xml and json formats.
+        """
+        self.is_fail = 0
+        self.run_cmd("lshw -xml")
+        self.run_cmd("lshw -html -class disk")
+        self.run_cmd("lshw -json")
+        if self.is_fail >= 1:
+            self.fail("%s command(s) failed to generate report"
+                      % self.fail_cmd)
+
+    def test_businfo(self):
+        """
+        Outputs the device list showing bus information,
+        detailing SCSI, USB, IDE and PCI addresses.
+        """
+        bus_info = process.run("lshw -businfo")
+        if bus_info.exit_status:
+            self.fail(" lshw  failed to execute lshw -businfo  ")
+
+        # verifying the bus info for active network
+        get_bus = process.system_output('lshw -businfo').splitlines()
+        for line in get_bus:
+            if self.active_interface in line:
+                get_bus_info_act_inter = line.split(' ')[0].split(':', 1)[1]
+
+        if get_bus_info_act_inter not in process.system_output("lspci -v "):
+            self.fail("Verification of network bus info failed ")
+
+    def test_sanitize(self):
+        """
+        sanitize output(remove sensitive information like serial numbers,etc.)
+        """
+        out_with_sanitize = process.system_output("lshw -sanitize")
+        for line in process.system_output("lshw").strip('\t\n\r').splitlines():
+            if ("serial:" in line) and (line in out_with_sanitize):
+                self.fail("Sensitive data is present in output")
+
+
+if __name__ == "__main__":
+    main()
