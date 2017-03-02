69791f198731bab2cb02147fc44d804875fa843a
Santhosh G <santhog4@linux.vnet.ibm.com>
2017-03-02 13:25:07 +0530

    Added basic test for THP.
    The test sets THP to always and stresses the system using
    dd command and checks whether THP has been allocated or not.
    
    Signed-off-by: Santhosh G <santhog4@linux.vnet.ibm.com>

---
+++ memory/transparent_hugepages.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+# Test Inspired basically from
+# https://github.com/autotest/tp-qemu/blob/master/generic/tests/trans_hugepage.py
+#
+# Copyright: 2017 IBM
+# Author: Santhosh G <santhog4@linux.vnet.ibm.com>
+
+import os
+from avocado import Test
+from avocado import main
+from avocado.utils import process
+from avocado.utils import memory
+from avocado.core import data_dir
+from avocado.utils.partition import Partition
+
+
+class Thp(Test):
+
+    '''
+    The test enables THP and stress the system using dd load
+    and verifies whether THP has been allocated for usage or not
+    '''
+
+    def setUp(self):
+
+        '''
+        Sets all the reqd parameter and also
+        mounts the device to be used in test.
+        '''
+
+        # Set params as per available memory in system
+        self.tmpdir = data_dir.get_tmp_dir()
+        self.mem_path = os.path.join(self.tmpdir, 'thp_space')
+        free_mem = int(memory.freememtotal() / 1024)
+        self.dd_timeout = 900
+
+        # Set block size as hugepage size * 2
+        self.block_size = (memory.get_huge_page_size() / 1024) * 2
+        self.count = free_mem / self.block_size
+
+        # Mount device as per free memory size
+        os.mkdir(self.mem_path)
+        self.device = Partition(device="none", mountpoint=self.mem_path)
+        self.device.mount(mountpoint=self.mem_path, fstype="tmpfs",
+                          args='-o size=%dM' % free_mem)
+
+    def test(self):
+
+        '''
+        Enables THP , Runs the dd workload and checks whether THP
+        has been allocated.
+        '''
+
+        # Enables THP
+        try:
+            memory.set_thp_value("enabled", "always")
+        except Exception as details:
+            self.fail("Failed  %s" % details)
+
+        # Read thp values before stressing the system
+        thp_alloted_before = int(memory.read_from_vmstat("thp_fault_alloc"))
+        thp_split_before = int(memory.read_from_vmstat("thp_split_page"))
+        thp_collapse_alloc_before = int(memory.read_from_vmstat
+                                        ("thp_collapse_alloc"))
+
+        # Start Stresssing the  System
+        self.log.info('Stress test Start')
+
+        for iterator in range(self.count):
+            stress_cmd = 'dd if=/dev/zero of=%s/%d bs=%dM count=1'\
+                         % (self.mem_path, iterator, self.block_size)
+            process.system(stress_cmd, timeout=self.dd_timeout,
+                           verbose=False, shell=True)
+
+        # Read thp values after stressing the system
+        thp_alloted_after = int(memory.read_from_vmstat("thp_fault_alloc"))
+        thp_split_after = int(memory.read_from_vmstat("thp_split_page"))
+        thp_collapse_alloc_after = int(memory.read_from_vmstat
+                                       ("thp_collapse_alloc"))
+
+        # Check whether THP is Used or not
+        if thp_alloted_after <= thp_alloted_before:
+            e_msg = "Thp usage count has not increased\n"
+            e_msg += "Before Stress:%d\nAfter stress:%d" % (thp_alloted_before,
+                                                            thp_alloted_after)
+            self.fail(e_msg)
+        else:
+            thp_fault_alloc = thp_alloted_after - thp_alloted_before
+            thp_split = thp_split_after - thp_split_before
+            thp_collapse_alloc = (thp_collapse_alloc_after -
+                                  thp_collapse_alloc_before)
+            self.log.info("\nTest statistics, changes during test run:")
+            self.log.info("thp_fault_alloc=%d\nthp_split=%d\n"
+                          "thp_collapse_alloc=%d\n",
+                          thp_fault_alloc, thp_split, thp_collapse_alloc)
+
+    def tearDown(self):
+
+        '''
+        Removes the files created and unmounts the device.
+        '''
+
+        if self.mem_path:
+            self.log.info('Cleaning Up!!!')
+            process.system('rm -rf %s/*' % self.mem_path)
+            self.device.unmount()
+
+
+if __name__ == "__main__":
+    main()
