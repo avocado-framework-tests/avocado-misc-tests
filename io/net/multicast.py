a97228f5e5e2d798eb1d928e91f832cc18c886ab
Harsha Thyagaraja <harshkid@linux.vnet.ibm.com>
2017-04-20 10:09:55 -0500

    Patch to fix ssh package conflict for SUSE distro
    patch fixes ssh package conflict for required tests
    under io/net directory
    
    Signed-off-by: Harsha Thyagaraja <harshkid@linux.vnet.ibm.com>

--- io/net/multicast.py
+++ io/net/multicast.py
@@ -41,6 +41,8 @@ class ReceiveMulticastTest(Test):
         detected_distro = distro.detect()
         if detected_distro.name == "Ubuntu":
             pkgs.append('openssh-client')
+        elif detected_distro.name == "SuSE":
+            pkgs.append('openssh')
         else:
             pkgs.append('openssh-clients')
         for pkg in pkgs:
