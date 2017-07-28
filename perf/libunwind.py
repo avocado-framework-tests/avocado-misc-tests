52f6bd1813726f20468b220a9411b13fe3472302
Pooja <pooja@linux.vnet.ibm.com>
2017-07-28 21:03:31 +0530

    Added test to run Libunwind tests_v1
    Removed self.fail and used self.cancel.
    
    Signed-off-by: Pooja <pooja@linux.vnet.ibm.com>

---
+++ perf/libunwind.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: 2017 IBM
+# Author: Pooja <pooja@linux.vnet.ibm.com>
+
+import os
+import re
+
+from avocado import Test
+from avocado import main
+from avocado.utils import archive, build, distro, process
+from avocado.utils.software_manager import SoftwareManager
+
+
+class Libunwind(Test):
+
+    def setUp(self):
+        '''
+        Build Libunwind library
+        Source:
+        https://github.com/pathscale/libunwind/archive/vanilla_pathscale.zip
+        '''
+        dist = distro.detect()
+        smm = SoftwareManager()
+        deps = ['gcc', 'libtool', 'autoconf', 'automake', 'make']
+        if dist.name == 'Ubuntu':
+            deps.extend(['dh-autoreconf', 'dh-dist-zilla', 'g++',
+                         'texlive-extra-utils'])
+        elif dist.name in ['SuSE', 'rhel', 'fedora', 'redhat']:
+            deps.extend(['gcc-c++'])
+        else:
+            self.cancel('Test not supported in distro')
+	
+        for package in deps:
+            if not smm.check_installed(package) and not smm.install(package):
+                self.cancel("Failed to install %s, which is needed for"
+                           "the test to be run" % package)
+
+        tarball = self.fetch_asset('vanilla_pathscale.zip', locations=[
+            'https://github.com/pathscale/libunwind/archive/'
+            'vanilla_pathscale.zip'], expire='7d')
+        archive.extract(tarball, self.srcdir)
+        self.srcdir = os.path.join(self.srcdir, 'libunwind-vanilla_pathscale')
+        os.chdir(self.srcdir)
+        process.run('./autogen.sh', shell=True)
+        '''
+        For configure options on different architecture please refer
+        https://github.com/pathscale/libunwind
+        '''
+        configure_option = self.params.get('configure_option',
+                                           default='configure_option')
+        process.run('./configure %s' % configure_option, shell=True)
+        build.make(self.srcdir)
+        build.make(self.srcdir, extra_args='install')
+
+    def test(self):
+        '''
+        Execute regression tests for libunwind library
+        '''
+        results = build.run_make(self.srcdir, extra_args='check',
+                                 ignore_status=True).stdout
+
+        fail_list = ['FAIL', 'XFAIL', 'ERROR']
+        failures = []
+        for failure in fail_list:
+            num_fails = re.compile(r"# %s:(.*)" %
+                                   failure).findall(results)[0].strip()
+            if int(num_fails):
+                failures.append({failure: num_fails})
+
+        if failures:
+            self.fail('Test failed with following:%s' % failures)
+
+
+if __name__ == "__main__":
+    main()
