f1fc9c30022240b12e0a72dfed67bc69f94e8d7c
Pavithra <pavrampu@linux.vnet.ibm.com>
2018-07-31 12:01:14 +0530

    Patch to test kdump with different smt value - v2
    This test sets smt to 2,4 and off, and test kdump.
    
    Signed-off-by: Pavithra <pavrampu@linux.vnet.ibm.com>, Nageswara R Sastry rnsastry@linux.vnet.ibm.com

---
+++ ras/features/steps/steps_kdump_smt.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from behave   import given, when, then
+#from hamcrest import assert_that, equal_to
+sys.path.append(os.path.abspath("../../"))
+from kdump import KDUMP
+
+@given('start_kdump')
+def step_impl(context):
+    context.a = KDUMP()
+
+@when('setup_kdump "{smt_value}"')
+def step_impl(context, smt_value):
+    context.a.setUp()
+    context.a.set_smt_value(smt_value)
+
+@then('test_kdump')
+def step_impl(context):
+    context.a.test()
