43fe74f74a181c4ba004eb4b730bbacee2b95e4b
Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
2018-09-19 13:43:31 +0530

    perf/generic_events: Add support for POWER9 events
    Generic Hardware (PMU) events codes differs for every platform,
    currently implementation might fails for event codes on POWER9, so
    make the logic configurable based on the underlying hardware. This
    patch reads the events from the configuration file, instead of using
    the hardcoded generic events.
    
    Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>

--- perf/perf_genericevents.py
+++ perf/perf_genericevents.py
@@ -16,37 +16,9 @@
 # Author: Shriya Kulkarni <shriyak@linux.vnet.ibm.com>
 
 import os
+import ConfigParser
 from avocado import Test
 from avocado import main
-
-
-def generic_events(x):
-    return {
-        'cpu-cycles': "0x1e",
-        'stalled-cycles-frontend': "0x100f8",
-        'stalled-cycles-backend': "0x4000a",
-        'instructions': "0x02",
-        'branch-instructions': "0x10068",
-        'branch-misses': "0x400f6",
-        'cache-references': "0x100ee",
-        'cache-misses': "0x3e054",
-        'L1-dcache-load-misses': "0x3e054",
-        'L1-dcache-loads': "0x100ee",
-        'L1-dcache-prefetches': "0xd8b8",
-        'L1-dcache-store-misses': "0x300f0",
-        'L1-icache-load-misses': "0x200fd",
-        'L1-icache-loads': "0x4080",
-        'L1-icache-prefetches': "0x408e",
-        'LLC-load-misses': "0x300fe",
-        'LLC-loads': "0x4c042",
-        'LLC-prefetches': "0x4e052",
-        'LLC-store-misses': "0x17082",
-        'LLC-stores': "0x17080",
-        'branch-load-misses': "0x400f6",
-        'branch-loads': "0x10068",
-        'dTLB-load-misses': "0x300fc",
-        'iTLB-load-misses': "0x400fc"
-    }.get(x, 9)
 
 
 class test_generic_events(Test):
@@ -57,14 +29,25 @@ class test_generic_events(Test):
     and compare the raw event code for each generic event
     """
 
+    def read_generic_events(self):
+        parser = ConfigParser.ConfigParser()
+        parser.optionxform = str
+        parser.read(self.get_data('raw_code.cfg'))
+        cpu_info = open('/proc/cpuinfo', 'r').read()
+        if 'POWER8' in cpu_info:
+            self.generic_events = dict(parser.items('POWER8'))
+        elif 'POWER9' in cpu_info:
+            self.generic_events = dict(parser.items('POWER9'))
+
     def test(self):
         nfail = 0
         dir = "/sys/bus/event_source/devices/cpu/events"
+        self.read_generic_events()
         os.chdir(dir)
         for file in os.listdir(dir):
             events_file = open(file, "r")
             event_code = events_file.readline()
-            val = generic_events(file)
+            val = self.generic_events.get(file, 9)
             raw_code = event_code.split('=', 2)[1].rstrip()
             if raw_code != val:
                 nfail += 1
