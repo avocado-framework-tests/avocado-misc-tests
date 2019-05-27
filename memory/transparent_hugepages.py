d0d2f5e0002b3a1731ead932c86f2e1f4d04ae53
Harish <harish@linux.vnet.ibm.com>
2019-05-27 18:19:48 +0530

    Use library functions to fetch machine configuration
    Patch replaces library functions to fetch machine configuration
    
    Signed-off-by: Harish <harish@linux.vnet.ibm.com>

--- memory/transparent_hugepages.py
+++ memory/transparent_hugepages.py
@@ -46,7 +46,7 @@ class Thp(Test):
 
         # Set params as per available memory in system
         self.mem_path = os.path.join(data_dir.get_tmp_dir(), 'thp_space')
-        free_mem = int(memory.freememtotal() / 1024)
+        free_mem = memory.meminfo.MemFree.m
         self.dd_timeout = 900
         self.thp_split = None
         try:
@@ -56,7 +56,7 @@ class Thp(Test):
             self.thp_split = "thp_split"
 
         # Set block size as hugepage size * 2
-        self.block_size = (memory.get_huge_page_size() / 1024) * 2
+        self.block_size = memory.meminfo.Hugepagesize.m * 2
         self.count = free_mem / self.block_size
 
         # Mount device as per free memory size
