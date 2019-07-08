15126b49cd2e9a1c411a687ead1ab9f54f86dc80
Harish <harish@linux.vnet.ibm.com>
2019-07-08 21:14:41 +0530

    Change print statements to log.info
    Patch replaces print statements to log.info so that we can avoid
    both python2 and python3 compatibility
    
    Signed-off-by: Harish <harish@linux.vnet.ibm.com>

--- ras/kdump_nfs.py
+++ ras/kdump_nfs.py
@@ -149,7 +149,7 @@ class KDUMP(Test):
                                             self.prompt_server, "\n", log_file_server, 100, 10, None)
         if self.distro == "rhel":
             nfs_dir_path = os.path.join(self.nfs_path, "var", "crash")
-        print nfs_dir_path
+        self.log.info(nfs_dir_path)
         session_check.run("ls -lrt %s;" % nfs_dir_path, 100, "True")
         crash_dir = self.run_cmd_out("cat %s | grep drwxr | awk '{print $NF}' | tail -1" % log_file_server)
         path_crash_dir = os.path.join(nfs_dir_path, crash_dir)
