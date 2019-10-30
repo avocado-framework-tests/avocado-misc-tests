369b99563cbacc69e8dffedef9a57bc41c739e07
Praveen K Pandey <praveen@linux.vnet.ibm.com>
2019-10-30 13:03:38 +0530

    Added Author signature in c file
    Added Author signature in c file
    
    Signed-off-by: Praveen K Pandey <praveen@linux.vnet.ibm.com>

--- perf/perf_UDT_probes.py.data/tick-main.c
+++ perf/perf_UDT_probes.py.data/tick-main.c
@@ -1,3 +1,18 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE for more details.
+ * Copyright: 2019 IBM
+ * Author: Naveen kumar T <naveet89@in.ibm.com>
+ */
+
+
 #include <stdio.h>
 #include <unistd.h>
 #include "tick-dtrace.h"
