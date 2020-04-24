64df53c192d352a3663289636034cfda052963c9
Praveen K Pandey <praveen@linux.vnet.ibm.com>
2020-04-24 14:14:19 +0530

    Fix issue as due sed command was not proper
    this PR fix issue https://github.com/avocado-framework-tests/avocado-misc-tests/issues/1632
    
    Signed-off-by: Praveen K Pandey <praveen@linux.vnet.ibm.com>

--- fs/flail.py
+++ fs/flail.py
@@ -42,7 +42,13 @@ class Flail(Test):
         archive.extract(self.get_data("flail-0.2.0.tar.gz"), self.workdir)
         self.build_dir = os.path.join(self.workdir, 'flail-0.2.0')
         os.chdir(self.build_dir)
-        process.system("sed -i 's/.*-lm.*/& -lm' Makefile")
+        fin = open("Makefile", "rt")
+        data = fin.read()
+        data = data.replace('-lm -o $@ flail.c', '-o $@ flail.c -lm')
+        fin.close()
+        fin = open("Makefile", "wt")
+        fin.write(data)
+        fin.close()
         build.make(self.build_dir)
 
     def test(self):
