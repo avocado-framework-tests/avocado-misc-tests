0e248e6fe2488d8451b9937a506ef21d017aec18
Narasimhan V <sim@linux.vnet.ibm.com>
2020-05-03 18:38:36 +0530

    Adding an option to run fio on raw disk and lv
    Adding an option to run fio on raw disk and lv.
    Now the test runs on a combination of lv, fs, raw disk.
    Also, some minor code path adjustments and enhancements.
    
    Signed-off-by: Narasimhan V <sim@linux.vnet.ibm.com>

--- io/disk/fiotest.py
+++ io/disk/fiotest.py
@@ -28,6 +28,8 @@ from avocado import Test
 from avocado import main
 from avocado.utils import archive
 from avocado.utils import build
+from avocado.utils import disk
+from avocado.utils import lv_utils
 from avocado.utils import process, distro
 from avocado.utils.partition import Partition
 from avocado.utils.software_manager import SoftwareManager
@@ -53,14 +55,7 @@ class FioTest(Test):
         default_url = "https://brick.kernel.dk/snaps/fio-git-latest.tar.gz"
         url = self.params.get('fio_tool_url', default=default_url)
         self.disk = self.params.get('disk', default=None)
-        self.dirs = self.params.get('dir', default=self.workdir)
-        fstype = self.params.get('fs', default='ext4')
-        tarball = self.fetch_asset(url)
-        archive.extract(tarball, self.teststmpdir)
-        self.sourcedir = os.path.join(self.teststmpdir, "fio")
-        build.make(self.sourcedir)
-
-        smm = SoftwareManager()
+        fstype = self.params.get('fs', default=None)
         if fstype == 'btrfs':
             ver = int(distro.detect().version)
             rel = int(distro.detect().release)
@@ -68,6 +63,9 @@ class FioTest(Test):
                 if (ver == 7 and rel >= 4) or ver > 7:
                     self.cancel("btrfs is not supported with \
                                 RHEL 7.4 onwards")
+        self.fs_create = False
+        lv_needed = self.params.get('lv', default=False)
+        self.lv_create = False
 
         if distro.detect().name in ['Ubuntu', 'debian']:
             pkg_list = ['libaio-dev']
@@ -76,26 +74,68 @@ class FioTest(Test):
         else:
             pkg_list = ['libaio', 'libaio-devel']
 
+        smm = SoftwareManager()
         for pkg in pkg_list:
             if pkg and not smm.check_installed(pkg) and not smm.install(pkg):
                 self.cancel("Package %s is missing and could not be installed"
                             % pkg)
 
-        if self.disk is not None:
-            self.part_obj = Partition(self.disk, mountpoint=self.dirs)
-            self.log.info("Unmounting disk/dir before creating file system")
-            self.part_obj.unmount()
-            self.log.info("creating file system")
-            self.part_obj.mkfs(fstype)
-            self.log.info("Mounting disk %s on directory %s",
-                          self.disk, self.dirs)
-            try:
-                self.part_obj.mount()
-            except PartitionError:
-                self.fail("Mounting disk %s on directory %s failed"
-                          % (self.disk, self.dirs))
+        if not self.disk:
+            self.disk = self.workdir
 
+        if self.disk in disk.get_disks():
+            if lv_needed:
+                self.disk = self.create_lv(self.disk)
+                self.lv_create = True
+                self.dirs = self.disk
+            else:
+                self.dirs = self.disk
+
+            if fstype:
+                self.dirs = self.workdir
+                self.create_fs(self.disk, self.dirs, fstype)
+                self.fs_create = True
+            else:
+                self.dirs = self.disk
+
+        else:
+            self.dirs = self.disk
+
+        tarball = self.fetch_asset(url)
+        archive.extract(tarball, self.teststmpdir)
+        self.sourcedir = os.path.join(self.teststmpdir, "fio")
+        build.make(self.sourcedir)
         self.fio_file = 'fiotest-image'
+
+    def create_lv(self, l_disk):
+        vgname = 'avocado_vg'
+        lvname = 'avocado_lv'
+        lv_size = int(lv_utils.get_diskspace(l_disk)) / 2330168
+        lv_utils.vg_create(vgname, disk)
+        lv_utils.lv_create(vgname, lvname, lv_size)
+        return '/dev/%s/%s' % (vgname, lvname)
+
+    def delete_lv(self):
+        vgname = 'avocado_vg'
+        lvname = 'avocado_lv'
+        lv_utils.lv_remove(vgname, lvname)
+        lv_utils.vg_remove(vgname)
+
+    def create_fs(self, l_disk, mountpoint, fstype):
+        self.part_obj = Partition(l_disk, mountpoint=mountpoint)
+        self.part_obj.unmount()
+        self.part_obj.mkfs(fstype)
+        try:
+            self.part_obj.mount()
+        except PartitionError:
+            self.fail("Mounting disk %s on directory %s failed"
+                      % (l_disk, mountpoint))
+
+    def delete_fs(self, l_disk):
+        self.part_obj.unmount()
+        delete_fs = "dd if=/dev/zero bs=512 count=512 of=%s" % l_disk
+        if process.system(delete_fs, shell=True, ignore_status=True):
+            self.fail("Failed to delete filesystem on %s" % l_disk)
 
     def test(self):
         """
@@ -106,20 +146,17 @@ class FioTest(Test):
         cmd = '%s/fio %s %s --filename=%s' % (self.sourcedir,
                                               self.get_data(fio_job),
                                               self.dirs, self.fio_file)
-        if process.system(cmd, ignore_status=True, shell=True) != 0:
+        if process.system(cmd, ignore_status=True, shell=True):
             self.fail("fio run failed")
 
     def tearDown(self):
         '''
         Cleanup of disk used to perform this test
         '''
-        if self.disk is not None:
-            self.log.info("Unmounting directory %s", self.dirs)
-            self.part_obj.unmount()
-        self.log.info("Removing the filesystem created on %s", self.disk)
-        delete_fs = "dd if=/dev/zero bs=512 count=512 of=%s" % self.disk
-        if process.system(delete_fs, shell=True, ignore_status=True):
-            self.fail("Failed to delete filesystem on %s" % self.disk)
+        if self.fs_create:
+            self.delete_fs(self.disk)
+        if self.lv_create:
+            self.delete_lv()
         if os.path.exists(self.fio_file):
             os.remove(self.fio_file)
 
