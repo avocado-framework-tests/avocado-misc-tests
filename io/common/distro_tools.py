7692f60edde04db978a574073a58c26faa8df6e9
bismurti bidhibrata pattajoshi <bbidhibr@in.ibm.com>
2020-05-14 15:43:12 +0530

    enhancement for disto_tools test and make separate yaml
    added few new tools to disto_tools and make separate yaml
    for pci, common and vitual tools
    
    Signed-off-by: bismurti bidhibrata pattajoshi <bbidhibr@in.ibm.com>
    Reported-by Abdul Haleem <abdhalee@linux.vnet.ibm.com>

---
+++ io/common/distro_tools.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Author: Bimurti Bidhibrata Pattjoshi <bbidhibr@in.ibm.com>
+#
+
+"""
+Test the different tools
+"""
+
+from avocado import main
+from avocado import Test
+from avocado.utils import process
+from avocado.utils import pci
+from avocado.utils.software_manager import SoftwareManager
+
+
+class DisrtoTool(Test):
+    '''
+    to test different type of tool
+    '''
+
+    def setUp(self):
+        '''
+        get all parameters
+        '''
+        self.option = self.params.get("test_opt", default='')
+        self.tool = self.params.get("tool", default='')
+        self.pci_device = self.params.get("pci_device", default=None)
+        if self.pci_device:
+            adapter_type = pci.get_pci_class_name(self.pci_device)
+            self.location_code = pci.get_slot_from_sysfs(self.pci_device)
+            self.interface = pci.get_interfaces_in_pci_address(self.pci_device,
+                                                               adapter_type)[0]
+            path = '/sys/class/net/%s/device/uevent' % self.interface
+            output = open(path, 'r').read()
+            for line in output.splitlines():
+                if "OF_FULLNAME" in line:
+                    self.device_path_name = line.split('=')[-1]
+        smm = SoftwareManager()
+        if not smm.check_installed("pciutils") and not smm.install("pciutils"):
+            self.cancel("pciutils package is need to test")
+
+    def test(self):
+        '''
+        test all distro tools
+        '''
+        cmd = "%s %s" % (self.tool, self.option)
+        if self.tool in ['usysident', 'usysattn'] and '-P' in cmd:
+            cmd += " -l %s" % self.location_code
+        elif self.tool == 'usysident' and '-t' in cmd:
+            cmd += " -d %s" % self.interface
+        elif self.tool == 'ofpathname' and '-l' in cmd:
+            cmd += " %s" % self.device_path_name
+        elif self.tool == 'ofpathname' and '-a' in cmd:
+            cmd += " %s" % self.interface
+
+        result = process.run(cmd, shell=True, ignore_status=True)
+        if self.tool == "usysident":
+            if "There is no identify indicator" in result.stdout_text:
+                self.log.warn("%s option %s failed" % (self.tool, self.option))
+        elif self.tool == "usysattn":
+            if "There is no fault indicator" in result.stdout_text:
+                self.log.warn("%s option %s failed" % (self.tool, self.option))
+        else:
+            if result.exit_status != 0:
+                self.fail("%s tool failed" % self.tool)
+
+
+if __name__ == "__main__":
+    main()
