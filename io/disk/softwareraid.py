78adddb35a28b61f6ab20ccd63c4ebcdb2d98f3a
Narasimhan V <sim@linux.vnet.ibm.com>
2020-06-22 22:35:02 +0530

    softwareraid: Overhaul using utils
    Moved all functions to utils, so that they can be used in
    other tests.
    
    Use case: avocado-misc-tests/issues/1674
    
    Signed-off-by: Narasimhan V <sim@linux.vnet.ibm.com>

--- io/disk/softwareraid.py
+++ io/disk/softwareraid.py
@@ -25,10 +25,9 @@ Some RAID levels include redundancy and so can survive some degree of device
 failure.
 """
 
-import time
 from avocado import Test
 from avocado.utils.software_manager import SoftwareManager
-from avocado.utils import process
+from avocado.utils import softwareraid
 
 
 class SoftwareRaid(Test):
@@ -40,7 +39,7 @@ class SoftwareRaid(Test):
     def setUp(self):
         """
         Checking if the required packages are installed,
-        if not found packages will be installed
+        if not found packages will be installed.
         """
 
         smm = SoftwareManager()
@@ -48,108 +47,46 @@ class SoftwareRaid(Test):
             self.log.info("Mdadm must be installed before continuing the test")
             if SoftwareManager().install("mdadm") is False:
                 self.cancel("Unable to install mdadm")
-        cmd = "mdadm -V"
-        self.check_pass(cmd, "Unable to get mdadm version")
-        self.disk = self.params.get('disks', default='').strip(" ")
-        self.raid = self.params.get('raidname', default='/dev/md/mdsraid')
-        self.raidlevel = str(self.params.get('raid', default='0'))
-        self.metadata = str(self.params.get('metadata', default='1.2'))
-        self.setup = self.params.get('setup', default=True)
-        self.run_test = self.params.get('run_test', default=False)
-        self.cleanup = self.params.get('cleanup', default=True)
-        self.sparedisk = ""
-        if self.raidlevel == 'linear' or self.raidlevel == '0':
-            self.disk_count = len(self.disk.split(" "))
-        else:
-            self.disk = self.disk.split(" ")
-            self.sparedisk = self.disk.pop()
-            self.remadd = ''.join(self.disk[-1:])
-            self.disk_count = len(self.disk)
-            self.disk = ' '.join(self.disk)
-        self.force = self.params.get('force', default=False)
+        disk = self.params.get('disks', default='').strip(" ")
+        if not disk:
+            self.cancel('No disks given')
+        disk = disk.split()
+        spare = self.params.get('spare_disks', default='').strip(" ")
+        if spare:
+            spare = spare.split()
+        raid = self.params.get('raidname', default='/dev/md/mdsraid')
+        raidlevel = str(self.params.get('raid', default='0'))
+        metadata = str(self.params.get('metadata', default='1.2'))
+        self.remadd = ''
+        if raidlevel not in ['0', 'linear']:
+            self.remadd = disk[-1]
+        self.sraid = softwareraid.SoftwareRaid(raid, raidlevel, disk,
+                                               metadata, spare)
 
-    def test_run(self):
+    def test(self):
         """
-        Decides which functions to be run for a perticular raid level
+        Decides which functions to be run for a perticular raid level, and runs
+        those tests.
         """
-        if self.setup:
-            self.create_raid()
-        if self.run_test:
-            if self.raidlevel != 'linear' and self.raidlevel != '0':
-                self.extensivetest()
 
-    def create_raid(self):
-        """
-        Only basic operations are run viz create and delete
-        """
-        cmd = "echo 'yes' | mdadm --create --verbose --assume-clean \
-            %s --level=%s --raid-devices=%d %s --metadata %s" \
-            % (self.raid, self.raidlevel, self.disk_count, self.disk,
-               self.metadata)
-        if self.sparedisk:
-            cmd += " --spare-devices=1 %s " % self.sparedisk
-        if self.force:
-            cmd += " --force"
-        self.check_pass(cmd, "Failed to create a MD device")
-        self.mdadm_detail()
-
-    def extensivetest(self):
-        """
-        Extensive software raid options are run viz create, delete, assemble,
-        create spares, remove and add drives
-        """
-        cmd = "mdadm --fail %s %s" % (self.raid, self.remadd)
-        self.check_pass(cmd, "Unable to fail a drive from MD device")
-        self.mdadm_detail()
-        cmd = "mdadm --manage %s --remove %s" % (self.raid, self.remadd)
-        self.check_pass(cmd, "Failed to remove a drive from MD device")
-        self.mdadm_detail()
-        cmd = "mdadm --manage %s --add %s" % (self.raid, self.remadd)
-        self.check_pass(cmd, "Failed to add back the drive to MD device")
-        self.mdadm_detail()
-        cmd = "mdadm --manage %s --stop" % self.raid
-        self.check_pass(cmd, "Failed to stop/remove the MD device")
-        cmd = "mdadm --assemble %s %s %s" \
-              % (self.raid, self.disk, self.sparedisk)
-        self.check_pass(cmd, "Failed to assemble back the MD device")
-        while self.is_mdadm_recovering():
-            time.sleep(30)
-        process.system(cmd, ignore_status=True, shell=True)
-        self.mdadm_detail()
-
-    def mdadm_detail(self):
-        """
-        Function to print the details of mdadm array
-        """
-        cmd = "mdadm --detail %s" % self.raid
-        output = process.run(cmd, ignore_status=True, shell=True)
-        if output.exit_status != 0:
-            self.fail("Failed to display MD device details")
-        return output.stdout.decode("utf-8").splitlines()
-
-    def is_mdadm_recovering(self):
-        """
-        Function to check if the array is recovering or not
-        """
-        for line in self.mdadm_detail():
-            if 'State' in line and 'recovering' in line:
-                return True
-        return False
-
-    def check_pass(self, cmd, errmsg):
-        """
-        Function to check if the cmd is successful or not, if not display
-        appropriate message
-        """
-        if process.system(cmd, ignore_status=True, shell=True) != 0:
-            self.fail(errmsg)
+        if not self.sraid.create():
+            self.fail("Failed to create")
+        if not self.remadd:
+            return
+        if not self.sraid.remove_disk(self.remadd):
+            self.fail("Failed to remove disk")
+        if not self.sraid.add_disk(self.remadd):
+            self.fail("Failed to add disk")
+        if not self.sraid.stop():
+            self.fail("Failed to stop raid")
+        if not self.sraid.assemble():
+            self.fail("Failed to assemble raid")
 
     def tearDown(self):
         """
-        Stop/Remove the MD device
+        Stop/Remove the raid device.
         """
-        if self.cleanup:
-            cmd = "mdadm --manage %s --stop" % self.raid
-            self.check_pass(cmd, "Failed to stop the MD device")
-            cmd = "mdadm --zero-superblock %s %s" % (self.disk, self.sparedisk)
-            self.check_pass(cmd, "Failed to remove the MD device")
+
+        if self.sraid:
+            self.sraid.stop()
+            self.sraid.clear_superblock()
