fb05bae8c597750a6f96c01074e5cd0a6e043060
bismurti bidhibrata pattajoshi <bbidhibr@in.ibm.com>
2020-07-30 14:07:50 +0530

    add LV, Software raid to disktest.py
    Test creates LV,software raid and runs test on them, as an option.
    
    Signed-off-by: bismurti bidhibrata pattajoshi <bbidhibr@in.ibm.com>

--- io/disk/disktest.py
+++ io/disk/disktest.py
@@ -29,8 +29,10 @@ from avocado import Test
 from avocado.utils import build
 from avocado.utils import memory
 from avocado.utils import process, distro
+from avocado.utils import disk
 from avocado.utils import lv_utils
 from avocado.utils.partition import Partition
+from avocado.utils import softwareraid
 from avocado.utils.software_manager import SoftwareManager
 from avocado.utils.partition import PartitionError
 
@@ -55,8 +57,8 @@ class Disktest(Test):
         :param chunk_mb: Size of the portion of the disk used to run the test.
                         Cannot be smaller than the total amount of RAM.
         """
-        softm = SoftwareManager()
-        if not softm.check_installed("gcc") and not softm.install("gcc"):
+        self.softm = SoftwareManager()
+        if not self.softm.check_installed("gcc") and not self.softm.install("gcc"):
             self.cancel('Gcc is needed for the test to be run')
         # Log of all the disktest processes
         self.disk_log = os.path.abspath(os.path.join(self.outputdir,
@@ -69,9 +71,14 @@ class Disktest(Test):
         """
         Retrieves and checks the test params
         """
+        self.fs_create = False
+        lv_needed = self.params.get('lv', default=False)
+        self.lv_create = False
+        raid_needed = self.params.get('raid', default=False)
+        self.raid_create = False
         self.disk = self.params.get('disk', default=None)
-        self.dirs = self.params.get('dir', default=self.workdir)
         self.fstype = self.params.get('fs', default='ext4')
+        self.dirs = self.disk
         if self.fstype == 'btrfs':
             ver = int(distro.detect().version)
             rel = int(distro.detect().release)
@@ -79,6 +86,10 @@ class Disktest(Test):
                 if (ver == 7 and rel >= 4) or ver > 7:
                     self.cancel("btrfs is not supported with \
                                 RHEL 7.4 onwards")
+        if raid_needed:
+            if not self.softm.check_installed("mdadm") \
+               and not self.softm.install("mdadm"):
+                self.cancel('mdadm is needed for the test to be run')
         gigabytes = lv_utils.get_device_total_space(self.disk) // 1073741824
         memory_mb = memory.meminfo.MemTotal.m
         self.chunk_mb = gigabytes * 950
@@ -95,17 +106,63 @@ class Disktest(Test):
                       self.dirs)
 
         if self.disk is not None:
-            self.part_obj = Partition(self.disk, mountpoint=self.dirs)
-            self.log.info("Unmounting the disk/dir if it is already mounted")
-            self.part_obj.unmount()
-            self.log.info("creating %s fs on %s", self.fstype, self.disk)
-            self.part_obj.mkfs(self.fstype)
-            self.log.info("mounting %s on %s", self.disk, self.dirs)
-            try:
-                self.part_obj.mount()
-            except PartitionError:
-                self.fail("Mounting disk %s on directory %s failed"
-                          % (self.disk, self.dirs))
+            if self.disk in disk.get_disks():
+                if raid_needed:
+                    raid_name = '/dev/md/mdsraid'
+                    self.create_raid(self.disk, raid_name)
+                    self.raid_create = True
+                    self.disk = raid_name
+                    self.dirs = self.disk
+
+                if lv_needed:
+                    self.disk = self.create_lv(self.disk)
+                    self.lv_create = True
+                    self.dirs = self.disk
+
+                if self.fstype:
+                    self.dirs = self.workdir
+                    self.create_fs(self.disk, self.dirs, self.fstype)
+                    self.fs_create = True
+
+    def create_raid(self, l_disk, l_raid_name):
+        self.sraid = softwareraid.SoftwareRaid(l_raid_name, '0',
+                                               l_disk.split(), '1.2')
+        self.sraid.create()
+
+    def delete_raid(self):
+        self.sraid.stop()
+        self.sraid.clear_superblock()
+
+    def create_lv(self, l_disk):
+        vgname = 'avocado_vg'
+        lvname = 'avocado_lv'
+        lv_size = lv_utils.get_device_total_space(l_disk) / 2330168
+        lv_utils.vg_create(vgname, l_disk)
+        lv_utils.lv_create(vgname, lvname, lv_size)
+        return '/dev/%s/%s' % (vgname, lvname)
+
+    def delete_lv(self):
+        vgname = 'avocado_vg'
+        lvname = 'avocado_lv'
+        lv_utils.lv_remove(vgname, lvname)
+        lv_utils.vg_remove(vgname)
+
+    def create_fs(self, l_disk, mountpoint, fstype):
+        self.part_obj = Partition(l_disk,
+                                  mountpoint=mountpoint)
+        self.part_obj.unmount()
+        self.part_obj.mkfs(fstype)
+        try:
+            self.part_obj.mount()
+        except PartitionError:
+            self.fail("Mounting disk %s on directory %s failed"
+                      % (l_disk, mountpoint))
+
+    def delete_fs(self, l_disk):
+        self.part_obj.unmount()
+        delete_fs = "dd if=/dev/zero bs=512 count=512 of=%s" % l_disk
+        if process.system(delete_fs, shell=True, ignore_status=True):
+            self.fail("Failed to delete filesystem on %s", l_disk)
 
     def _compile_disktest(self):
         """
@@ -156,10 +213,9 @@ class Disktest(Test):
             for filename in glob.glob("%s/testfile.*" % disk):
                 os.remove(filename)
         if self.disk is not None:
-            self.log.info("Unmounting disk %s on directory %s",
-                          self.disk, self.dirs)
-            self.part_obj.unmount()
-        self.log.info("Removing the filesystem created on %s", self.disk)
-        delete_fs = "dd if=/dev/zero bs=512 count=512 of=%s" % self.disk
-        if process.system(delete_fs, shell=True, ignore_status=True):
-            self.fail("Failed to delete filesystem on %s", self.disk)
+            if self.fs_create:
+                self.delete_fs(self.disk)
+            if self.lv_create:
+                self.delete_lv()
+            if self.raid_create:
+                self.delete_raid()
