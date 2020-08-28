2cbf15083eb745dea19fd5851a54335db104b191
yiwei075 <yiwei075@qq.com>
2020-08-28 10:28:18 +0800

    add tainting kernel
    Signed-off-by: yiwei075 <yiwei075@qq.com>

--- fuzz/trinity.py
+++ fuzz/trinity.py
@@ -94,7 +94,7 @@ class Trinity(Test):
         match = re.search(br'Call Trace:', dmesg, re.M | re.I)
         if match:
             self.log.info("some call traces seen please check")
-        match = re.search(br'tainting:', dmesg, re.M | re.I)
+        match = re.search(br'tainting', dmesg, re.M | re.I)
         if match:
             self.log.info("kernel become tainting please check")
 
