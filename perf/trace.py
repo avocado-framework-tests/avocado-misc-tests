4b3d5d92450c6e62d98a75040c14989ffe43e599
Kalpana Shetty <kalshett@in.ibm.com>
2021-03-19 07:23:49 +0530

    trace: trace test patch review comments
    Incorporated trace test cases review comments
    
    Signed-off-by: Kalpana Shetty <kalshett@in.ibm.com>

--- perf/trace.py
+++ perf/trace.py
@@ -28,10 +28,16 @@ class Linuxtrace(Test):
     Linux Trace tests
     """
 
+    failed_tests = list()
     @staticmethod
-    def run_cmd_out(cmd):
-        return process.system_output(cmd, shell=True, ignore_status=True,
-                                     sudo=True)
+    def run_cmd_out(self, cmd):
+        logfile = "/tmp/ftracetest.log"
+        process.system_output(cmd, ignore_status=True)
+        with open(logfile, 'r') as file_p:
+            lines = file_p.readlines()
+            for line in lines:
+                if 'FAIL' in line:
+                    self.failed_tests.append(line)
 
     def setUp(self):
         '''
@@ -39,39 +45,15 @@ class Linuxtrace(Test):
         '''
         self.testdir = "tools/testing/selftests/ftrace"
         smg = SoftwareManager()
-        self.run_type = self.params.get('type', default='upstream')
+        self.version = self.params.get('type', default='upstream')
         detected_distro = distro.detect()
 
-        if self.run_type == 'distro':
+        if self.version == 'distro':
             # Make sure kernel source repo is configured
             if detected_distro.name in ['rhel', 'centos']:
-                deps = ['yum-utils']
-                for package in deps:
-                    if not smg.check_installed(package) and not\
-                            smg.install(package):
-                        self.cancel("Fail to install %s\
-                                    required for this test."
-                                    % (package))
-                uname_cmd = "uname -r | cut -f 1-4 -d '.'"
-                self.output = self.run_cmd_out("%s" % uname_cmd).decode(
-                                               "utf-8")
-                kernel_src = ("kernel-%s.src" % self.output)
-
-                download_cmd = ("yumdownloader --assumeyes --verbose\
-                                --source %s --destdir %s" %
-                                (kernel_src, self.workdir))
-                self.run_cmd_out("%s" % download_cmd)
-
-                install_rpm_cmd = ("rpm -i %s/%s.rpm" %
-                                   (self.workdir, kernel_src))
-                self.run_cmd_out("%s" % install_rpm_cmd)
-
-                src_def_path = "/root/rpmbuild/SOURCES"
-                linux_name = "linux-%s.tar.xz" % self.output
-                tarball = "%s/%s" % (src_def_path, linux_name)
-                archive.extract(tarball, self.workdir)
-                os.chdir("%s/linux-%s/%s" % (self.workdir, self.output,
-                         self.testdir))
+                self.buldir = smg.get_source("kernel", self.workdir)
+                self.buldir = os.path.join(
+                    self.buldir, os.listdir(self.buldir)[0])
             elif 'SuSE' in detected_distro.name:
                 if not smg.check_installed("kernel-source") and not\
                         smg.install("kernel-source"):
@@ -79,12 +61,11 @@ class Linuxtrace(Test):
                         "Failed to install kernel-source for this test.")
                 if not os.path.exists("/usr/src/linux"):
                     self.cancel("kernel source missing after install")
-                self.output = "/usr/src/linux"
-                os.chdir("%s/%s" % (self.output, self.testdir))
+                self.buldir = "/usr/src/linux"
         else:
             location = self.params.get('location', default='https://github.c'
                                        'om/torvalds/linux/archive/master.zip')
-            self.output = "master"
+            self.output = "linux-master"
 
             match = next(
                 (ext for ext in [".zip", ".tar"] if ext in location), None)
@@ -94,47 +75,16 @@ class Linuxtrace(Test):
                 archive.extract(tarball, self.workdir)
             else:
                 git.get_repo(location, destination_dir=self.workdir)
-            os.chdir("%s/linux-%s/%s" % (self.workdir, self.output,
-                     self.testdir))
-
-    def test_ftrace_basic(self):
-        '''
-        Execute ftrace basic tests
-        '''
-        self.log.info("ftrace basic tests...")
-        self.run_cmd_out("./ftracetest ./test.d/00basic")
-
-    def test_ftrace_event(self):
-        '''
-        Execute ftrace event tests
-        '''
-        self.log.info("ftrace event tests....")
-        self.run_cmd_out("./ftracetest test.d/event")
+            self.buldir = os.path.join(self.workdir, self.output)
+        os.chdir(os.path.join(self.buldir, self.testdir))
 
     def test_ftrace(self):
         '''
         Execute ftrace tests
         '''
-        self.log.info("ftrace tests....")
-        self.run_cmd_out("./ftracetest test.d/ftrace")
+        self.args = self.params.get('args', default='')
+        self.log.info("ftrace tests...: %s" % self.args)
+        self.run_cmd_out(self, "./ftracetest -l /tmp ./test.d/%s" % self.args)
 
-    def test_ftrace_instances(self):
-        '''
-        Execute ftrace instances tests
-        '''
-        self.log.info("ftrace instaces tests....")
-        self.run_cmd_out("./ftracetest test.d/instances")
-
-    def test_ftrace_trigger(self):
-        '''
-        Execute ftrace trigger tests
-        '''
-        self.log.info("ftrace trigger tests....")
-        self.run_cmd_out("./ftracetest test.d/trigger")
-
-    def test_ftrace_kprobe(self):
-        '''
-        Execute ftrace kprobe tests
-        '''
-        self.log.info("ftrace kprobe tests....")
-        self.run_cmd_out("./ftracetest test.d/kprobe")
+        if self.failed_tests:
+            self.fail("Failed Tests: %s" % self.failed_tests)
