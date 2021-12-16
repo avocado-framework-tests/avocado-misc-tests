375d530c3c77de670eeae99bfe676318aa7cab35
Kalpana Shetty <kalshett@in.ibm.com>
2021-12-16 12:34:43 +0530

    Added dlpar cpu/memory test cases
    Dlpar cpu/memory test cases which coveres ded/shared lpar configuration with add/remove/move dlpar operations.
    
    Signed-off-by: Kalpana Shetty <kalshett@in.ibm.com>

---
+++ dlpar/dedicated_cpu.py
@@ -0,0 +1,285 @@
+#!/usr/bin/env python
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: 2021 IBM
+# Author(Original): Ricardo Salveti <rsalveti@linux.vnet.ibm.com>
+# Author(Modified): Kalpana Shetty <kalshett@in.ibm.com>
+
+"""
+Test to verify dedicated CPU DLPAR. Operations tested:
+ * Add
+ * Move
+ * Remove
+
+This test assumes that we have 2 power LPARS properly configured to start.
+"""
+import time
+from dlpar_api.api import TestCase, TestException
+
+
+ 
+__all__ = ['DedicatedCpu']
+
+class DedicatedCpu(TestCase):
+    """
+    DLPAR Dedicated CPU test case.
+
+    The test procedure:
+     1 - Get how many dedicated processors to test;
+     2 - Check and set the test environment for both linux partitions;
+     3 - Test (see run_test())
+
+    Everything is fine if we don't have troubles with the HMC and the linux
+    partitions are recognizing all added/removed cpus 
+    (using dmesg and /var/log/messages).
+    """
+
+    def __prep_ded_cfg(self, linux_machine):
+        """
+         Activate dedicated partition with the user defined min/desired/max
+
+         Check:
+          1 - Shutdown the partition (dedicated);
+          2 - Define dedicated partition with min, desired, max from config 
+        """
+        u_cmd = 'chsyscfg -r prof -m %s -p %s -i \
+                "lpar_name=%s,name=default_profile,proc_mode=ded, \
+                min_procs=%s,desired_procs=%s,max_procs=%s,sharing_mode=keep_idle_procs" \
+                --force' % (linux_machine.machine,linux_machine.partition, \
+                linux_machine.name,self.min_procs,self.desired_procs,self.max_procs)
+        self.log.info('DEBUG: Dedicated lpar setup %s' % u_cmd)
+        self.hmc.sshcnx.run_command(u_cmd, False)
+
+        d_cmd = 'chsysstate -m %s -o shutdown -r lpar -n %s --immed' % \
+                (linux_machine.machine,linux_machine.name)
+        self.log.info('DEBUG: Dedicated lpar setup %s' % d_cmd)
+        self.hmc.sshcnx.run_command(d_cmd, False)
+        time.sleep(20)
+
+        a_cmd = 'chsysstate -m %s -r lpar -o on -n %s -f default_profile \
+                --force' % (linux_machine.machine,linux_machine.name)
+        self.log.info('DEBUG: Dedicated lpar setup %s' % a_cmd)
+        self.hmc.sshcnx.run_command(a_cmd, False)
+        time.sleep(120)
+
+
+    def __init__(self, log = 'dedicated_cpu.log'):
+        TestCase.__init__(self, log, 'Dedicated CPU')
+
+        self.get_connections()
+
+        # Get test configuration
+        self.quant_to_test = int(self.config.get('dedicated_cpu',
+                                                 'quantity_to_test'))
+        self.sleep_time = int(self.config.get('dedicated_cpu',
+                                              'sleep_time'))
+        self.iterations = int(self.config.get('dedicated_cpu',
+                                              'iterations'))
+        self.min_procs = int(self.config.get('dedicated_cpu',
+                                              'min_procs'))
+        self.desired_procs = int(self.config.get('dedicated_cpu',
+                                              'desired_procs'))
+        self.max_procs = int(self.config.get('dedicated_cpu',
+                                              'max_procs'))
+        self.log.check_log('Getting Test configuration.', 
+                           self.quant_to_test != None)
+        self.log.debug("Testing with %s Dedicated CPU units." % \
+                       self.quant_to_test)
+
+        # shutdown the paritition, update profile with min,desired,max, activate
+        self.__prep_ded_cfg(self.linux_1)
+        self.__prep_ded_cfg(self.linux_2)
+
+        self.get_connections()
+
+        # Check linux partitions configuration
+        self.__check_set_cfg(self.linux_1)
+        self.__check_set_cfg(self.linux_2)
+
+    def __check_set_cfg(self, linux_machine):
+        """
+        Test and set machine configuration, to see if we can actually
+        perform the test.
+
+        Check:
+         1 - Processor type (dedicated, shared);
+         2 - Max cpus;
+         3 - Have enought processor units; 
+        """
+        self.log.info("Checking partition '%s' configuration." % \
+                      linux_machine.partition)
+        self.log.debug("Machine: %s" % linux_machine.name)
+
+        # Checking processor type
+        curr_proc_mode = self.get_cpu_option(linux_machine, 'curr_proc_mode')
+        d_msg = "Checking if linux partition '%s' is at dedicated mode" % \
+                linux_machine.partition
+        self.log.info('DEBUG: curr_proc_node %s' % curr_proc_mode)
+        d_condition = (curr_proc_mode == "ded")
+        self.log.check_log(d_msg, d_condition)
+
+        # Get cpu configuration
+        curr_min_procs = int(self.get_cpu_option(linux_machine,
+                                                 'curr_min_procs'))
+        curr_procs = int(self.get_cpu_option(linux_machine, 'curr_procs'))
+
+        # Check dedicated cpu quantity
+
+        ## Set the curr_procs to curr_min_procs if we need it
+        if curr_min_procs != curr_procs:
+            s_msg = 'Setting the curr_procs to curr_min_procs at %s' \
+                    % linux_machine.partition
+            self.log.debug(s_msg)
+            m_cmd = 'chhwres -m ' + linux_machine.machine + \
+                    ' -r proc -o r --procs ' + \
+                    str(curr_procs - curr_min_procs) + \
+                    ' -p "' + linux_machine.partition + '"'
+            self.hmc.sshcnx.run_command(m_cmd, False)
+
+            self.log.debug('Sleeping for %s seconds before proceeding' %
+                           self.sleep_time)
+            time.sleep(self.sleep_time)
+
+            m_msg = 'Removing %s dedicated cpus form partition %s.' % \
+                    (curr_procs - curr_min_procs, linux_machine.partition)
+            m_condition = int(self.get_cpu_option(linux_machine, 
+                                                  'curr_procs')) == \
+                              curr_min_procs
+            self.log.check_log(m_msg, m_condition)
+
+        o_msg = 'Configuration settings for partition %s all correct.' % \
+                linux_machine.partition
+        self.log.info(o_msg)
+
+
+    def run_test(self):
+        """Run the test.
+
+        1 - Add X dedicated cpus to first partition;
+        2 - Move X dedicated cpus from the first partition to the second one;
+        3 - Remove X dedicated cpus from the second partitions;
+        4 - Add X dedicated cpus to the second partition;
+        5 - Move X dedicated cpus from the second partition to the first one;
+        6 - Remove X dedicated cpus from the first partition;
+        """
+        self.log.info("Initiating the test.")
+        for iteration in range(1, self.iterations + 1):
+            self.log.info("Running iteration %d" % iteration)
+            self.__add_dedicated_cpu(self.linux_1, self.quant_to_test)
+            self.__move_dedicated_cpu(self.linux_1, self.linux_2,
+                                      self.quant_to_test)
+            self.__remove_dedicated_cpu(self.linux_2, self.quant_to_test)
+            self.__add_dedicated_cpu(self.linux_2, self.quant_to_test)
+            self.__move_dedicated_cpu(self.linux_2, self.linux_1,
+                                      self.quant_to_test)
+            self.__remove_dedicated_cpu(self.linux_1, self.quant_to_test)
+        self.log.info("Test finished successfully :)")
+
+
+    def __add_dedicated_cpu(self, linux_machine, quantity):
+        """Add 'quantity' dedicated cpus at the a linux partition."""
+        # Get all values before adding
+        curr_procs_before = int(self.get_cpu_option(linux_machine,
+                                                    'curr_procs'))
+        time.sleep(self.sleep_time)
+
+        # Add the cpus
+        a_cmd = 'chhwres -m ' + linux_machine.machine + \
+                ' -r proc -o a --procs ' + str(quantity) + \
+                ' -p "' + linux_machine.partition + '"'
+        cmd_result = self.hmc.sshcnx.run_command(a_cmd)
+        self.log.debug('Sleeping for %s seconds before proceeding' %
+                       self.sleep_time)
+        time.sleep(self.sleep_time)
+
+        # Check at HMC
+        a_msg = 'Adding %d dedicated cpus to partition %s.' % \
+                (quantity, linux_machine.partition)
+        a_condition = int(self.get_cpu_option(linux_machine, 'curr_procs')) == \
+                      curr_procs_before + quantity
+        if not self.log.check_log(a_msg, a_condition, False):
+            e_msg = 'Error adding %d dedicated cpus to partition %s.' % \
+                    (quantity, linux_machine.partition)
+
+            self.log.error(e_msg)
+            raise TestException(cmd_result)
+
+        ## Check at Linux Partition
+        self.linux_check_add_cpu(linux_machine, quantity)
+
+
+    def __move_dedicated_cpu(self, linux_machine_1, linux_machine_2, quantity):
+        """
+        Move 'quantity' dedicated cpus from linux_machine_1 to linux_machine_2.
+        """
+        # Get all values from both machines before moving
+        curr_procs_before_1 = int(self.get_cpu_option(linux_machine_1,
+                                                      'curr_procs'))
+        curr_procs_before_2 = int(self.get_cpu_option(linux_machine_2,
+                                                      'curr_procs'))
+        # Move the processors
+        m_cmd = 'chhwres -m ' + linux_machine_1.machine + \
+                ' -r proc -o m --procs ' + str(quantity) + \
+                ' -p "' + linux_machine_1.partition + '"' + \
+                ' -t "' + linux_machine_2.partition + '"'
+        cmd_result = self.hmc.sshcnx.run_command(m_cmd)
+        self.log.debug('Sleeping for %s seconds before proceeding' %
+                       self.sleep_time)
+        time.sleep(self.sleep_time)
+        # Check at HMC
+        m_msg = 'Moving %s dedicated cpus from %s to %s.' % \
+                (quantity, linux_machine_1.partition, linux_machine_2.partition)
+        m_condition = (int(self.get_cpu_option(linux_machine_1, 'curr_procs')) \
+                       == curr_procs_before_1 - quantity) \
+                       and (int(self.get_cpu_option(linux_machine_2, 
+                                                    'curr_procs')) \
+                       == curr_procs_before_2 + quantity)
+        if not self.log.check_log(m_msg, m_condition, False):
+            self.log.error(cmd_result)
+            raise TestException(cmd_result)
+
+        # Check at both linux partitions
+        self.linux_check_rm_cpu(linux_machine_1, curr_procs_before_1, quantity)
+        self.linux_check_add_cpu(linux_machine_2, quantity)
+
+    def __remove_dedicated_cpu(self, linux_machine, quantity):
+        """Remove 'quantity' dedicated cpus from linux_machine."""
+        # Get all values before removing
+        curr_procs_before = int(self.get_cpu_option(linux_machine,
+                                                    'curr_procs'))
+        # Remove the cpus
+        r_cmd = 'chhwres -m ' + linux_machine.machine + \
+                ' -r proc -o r --procs ' + str(quantity) + \
+                ' -p "' + linux_machine.partition + '"'
+        self.hmc.sshcnx.run_command(r_cmd)
+        self.log.debug('Sleeping for %s seconds before proceeding' %
+                       self.sleep_time)
+        time.sleep(self.sleep_time)
+        # Check at HMC
+        r_msg = 'Removing %s dedicated cpus from partition %s.' % \
+                (quantity, linux_machine.partition)
+        r_condition = int(self.get_cpu_option(linux_machine, 'curr_procs')) == \
+                      (curr_procs_before - quantity)
+        if not self.log.check_log(r_msg, r_condition, False):
+            e_msg = 'Error removing %s dedicated cpus from partition %s.' % \
+                    (quantity, linux_machine.partition)
+
+            self.log.error(e_msg)
+            raise TestException(e_msg)
+
+        ## Check at Linux Partition
+        self.linux_check_rm_cpu(linux_machine, curr_procs_before, quantity)
+
+if __name__ == "__main__":
+    DEDICATED_CPU = DedicatedCpu()
+    DEDICATED_CPU.run_test()
---
+++ dlpar/memory.py
@@ -0,0 +1,329 @@
+#!/usr/bin/env python
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: 2021 IBM
+# Author: Ricardo Salveti <rsalveti@linux.vnet.ibm.com>
+# Author(Modified): Kalpana Shetty <kalshett@in.ibm.com>
+
+"""
+Test to verify memory DLPAR. Operations tested:
+ * Add
+ * Move
+ * Remove
+
+This test assumes that we have 2 power LPARS properly configured to start.
+"""
+import time
+from dlpar_api.api import TestCase, TestException
+
+__all__ = ['Memory']
+
+class Memory(TestCase):
+    """DLPAR memory add/move/remove test case.
+
+   The test procedure:
+    1 - Get how much of memory to test;
+    2 - Check the test environment for both linux partitions;
+    3 - Test (see run_test())
+
+    Everything is fine if we don't have troubles with the HMC and the linux 
+    partitions are recognizing all added memory (using /proc/meminfo).
+    """
+
+    def __prep_mem_cfg(self, linux_machine):
+        """
+         Activate shared partition with the user defined min/desired/max
+
+         Check:
+          1 - Shutdown the partition (shared);
+          2 - Define memory profile with min, desired, max from config
+        """
+        u_cmd = 'chsyscfg -r prof -m %s -p %s -i "lpar_name=%s,name=default_profile, \
+                min_mem=%d,desired_mem=%d,max_mem=%d, \
+                min_num_huge_pages=0,desired_num_huge_pages=0, \
+                max_num_huge_pages=0" --force' % \
+                (linux_machine.machine,linux_machine.partition,linux_machine.name, \
+                self.min_mem,self.desired_mem,self.max_mem)
+
+        self.log.info('DEBUG: Memory lpar setup %s' % u_cmd)
+        self.hmc.sshcnx.run_command(u_cmd, False)
+
+        d_cmd = 'chsysstate -m %s -o shutdown -r lpar -n %s --immed' % \
+                (linux_machine.machine,linux_machine.name)
+        self.log.info('DEBUG: Memory lpar setup %s' % d_cmd)
+        self.hmc.sshcnx.run_command(d_cmd, False)
+        time.sleep(20)
+
+        a_cmd = 'chsysstate -m %s -r lpar -o on -n %s -f default_profile \
+                --force' % (linux_machine.machine,linux_machine.name)
+        self.log.info('DEBUG: Memory lpar setup %s' % a_cmd)
+        self.hmc.sshcnx.run_command(a_cmd, False)
+        time.sleep(120)
+
+    def __init__(self, log = 'memory.log'):
+        TestCase.__init__(self, log, 'Memory')
+
+        # Get test configuration
+        self.quant_to_test = int(self.config.get('memory',
+                                                 'quantity_to_test'))
+        self.sleep_time = int(self.config.get('memory', 'sleep_time'))
+        self.iterations = int(self.config.get('memory', 'iterations'))
+
+        self.min_mem = int(self.config.get('memory','min_mem'))
+        self.desired_mem = int(self.config.get('memory','desired_mem'))
+        self.max_mem = int(self.config.get('memory','max_mem'))
+
+        # shutdown the paritition, update profile with min,desired,max, activate
+        self.get_connections()
+        self.__prep_mem_cfg(self.linux_1)
+        self.__prep_mem_cfg(self.linux_2)
+        self.get_connections()
+
+        self.mode = self.config.get('memory', 'mode')
+        if self.mode == 'add' or self.mode == 'add_remove':
+            self.linux_machine = self.config.get('memory', 'linux_machine')
+            if self.linux_machine == 'primary':
+                self.get_connections(clients='primary')
+                self.linux = self.linux_1
+            elif self.linux_machine == 'secondary':
+                self.get_connections(clients='secondary')
+                self.linux = self.linux_2
+            else:
+                e_msg_1 = "Invalid 'linux_machine' at the configuration file."
+                e_msg_2 = "Please use either 'primary' or 'secondary'."
+                self.log.error(e_msg_1)
+                self.log.error(e_msg_2)
+                raise TestException(e_msg_1 + " " + e_msg_2)
+        elif self.mode == 'add_move_remove':
+            self.get_connections()
+        else:
+            e_msg_1 = "Invalid 'mode' at the configuration file."
+            e_msg_2 = "Use one of 'add', 'add_remove', 'add_move_remove'."
+            self.log.error(e_msg_1)
+            self.log.error(e_msg_2)
+            raise TestException(e_msg_1 + " " + e_msg_2)
+
+        self.log.debug("Testing with %s megabytes of memory." % \
+                       self.quant_to_test)
+        c_msg = 'Getting Test configuration.'
+        c_condition = self.quant_to_test != None
+        self.log.check_log(c_msg, c_condition)
+
+        # Check linux partitions configuration
+        try:
+            self.__check_set_cfg(self.linux_1)
+        except AttributeError:
+            self.log.debug('linux_1 not found, so not checking it.')
+
+        try:
+            self.__check_set_cfg(self.linux_2)
+        except AttributeError:
+            self.log.debug('linux_2 not found, so not checking it.')
+
+
+    def __check_set_cfg(self, linux_machine):
+        """Test the machine configuration, to see if we can do the test.
+
+        Check:
+        1 - If machine has enough memory to add;
+        2 - If profile supports adding 'quant_to_test' of memory;
+        """
+        self.log.info("Checking partition '%s' configuration." % \
+                      linux_machine.partition)
+        self.log.debug("Machine: %s" % linux_machine.name)
+
+        # Getting memory configuration
+        m_cmd = 'lshwres -m ' + linux_machine.machine + \
+                ' --level sys -r mem -F curr_avail_sys_mem'
+        curr_avail_sys_mem = int(self.hmc.sshcnx.run_command(m_cmd))
+        curr_max_mem = int(self.get_mem_option(linux_machine, 'curr_max_mem'))
+        curr_mem = int(self.get_mem_option(linux_machine, 'curr_mem'))
+
+        ## Check if the system support the memory units to add
+        m_msg = "Checking if the system has enough available memory to add."
+        m_condition = self.quant_to_test <= curr_avail_sys_mem
+        self.log.check_log(m_msg, m_condition)
+
+        p_msg = "Checking if the system's profile supports %s of memory." % \
+                (self.quant_to_test + curr_mem)
+        p_condition = curr_mem + self.quant_to_test <= curr_max_mem
+        self.log.check_log(p_msg, p_condition)
+
+        self.log.info('Configuration data for LPAR %s is OK.' % \
+                      linux_machine.partition)
+
+
+    def run_test(self):
+        """Run the test.
+
+        1 - Add 'quantity' of memory to the first partition;
+        2 - Move 'quantity' of memory from the first to the second partition;
+        3 - Remove 'quantity' of memory from the second partition;
+
+        4 - Repeat steps 1 to 3, this time starting from the second partition.
+        """
+        self.log.info("Initiating the test.")
+        for iteration in range(1, self.iterations + 1):
+            self.log.info("Running iteration %d" % iteration)
+            if self.mode == 'add':
+                self.__add_memory(self.linux, self.quant_to_test)
+            elif self.mode == 'add_remove':
+                self.__add_memory(self.linux, self.quant_to_test)
+                self.__remove_memory(self.linux, self.quant_to_test)
+            elif self.mode == 'add_move_remove':
+                self.__add_memory(self.linux_1, self.quant_to_test)
+                self.__move_memory(self.linux_1, self.linux_2,
+                                   self.quant_to_test)
+                self.__remove_memory(self.linux_2, self.quant_to_test)
+
+                self.__add_memory(self.linux_2, self.quant_to_test)
+                self.__move_memory(self.linux_2, self.linux_1,
+                                   self.quant_to_test)
+                self.__remove_memory(self.linux_1, self.quant_to_test)
+
+        self.log.info("Test finished successfully.")
+
+
+    def __check_memory_proc(self, machine):
+        """
+        Check on a linux client how much physical memory we have.
+        """
+        p_cmd = "cat /proc/meminfo | grep MemTotal | awk -F' ' '{ print $2 }'"
+        self.log.info('Checking memory under linux on %s' % machine.partition)
+        self.log.debug('Checking memory under linux on %s' % machine.partition)
+        memory = int(machine.sshcnx.run_command(p_cmd))
+        return memory
+
+
+    def __add_memory(self, linux_machine, quantity):
+        """
+        Add 'quantity' of memory at the linux partition.
+        """
+        # Get all values before adding
+        curr_mem_before = int(self.get_mem_option(linux_machine, 'curr_mem'))
+        curr_mem_proc_before = self.__check_memory_proc(linux_machine)
+
+        # Add the memory
+        a_cmd = 'chhwres -m ' + linux_machine.machine + ' -r mem -o a -q ' + \
+                str(quantity) + ' -p "' + linux_machine.partition + '"'
+        self.hmc.sshcnx.run_command(a_cmd)
+
+        self.log.debug('Sleeping for %s seconds before proceeding' %
+                       self.sleep_time)
+        time.sleep(self.sleep_time)
+
+        # Check at HMC
+        a_msg = 'Adding %d megabytes of memory to partition %s.' % \
+                (quantity, linux_machine.partition)
+        a_condition = (int(self.get_mem_option(linux_machine, 'curr_mem')) == \
+                       curr_mem_before + quantity)
+        if not self.log.check_log(a_msg, a_condition, False):
+            e_msg = 'Error happened when adding memory to %s' % \
+                    linux_machine.partition
+            self.log.error(e_msg)
+            raise TestException(e_msg)
+
+        # Check at Linux partition (using /proc/meminfo)
+        curr_mem_proc_after = self.__check_memory_proc(linux_machine)
+        p_msg = 'Checking if /proc/meminfo shows all added memory.'
+        p_condition = ((curr_mem_proc_after - curr_mem_proc_before)/1024 == \
+                      quantity)
+        self.log.check_log(p_msg, p_condition)
+
+
+    def __remove_memory(self, linux_machine, quantity):
+        """Remove 'quantity' of memory from a linux partition."""
+        # Get all values before adding
+        curr_mem_before = int(self.get_mem_option(linux_machine, 'curr_mem'))
+        curr_mem_proc_before = self.__check_memory_proc(linux_machine)
+
+        # Remove the memory
+        r_cmd = 'chhwres -m ' + linux_machine.machine + ' -r mem -o r -q ' + \
+                str(quantity) + ' -p "' + linux_machine.partition + '"'
+        self.hmc.sshcnx.run_command(r_cmd)
+        self.log.debug('Sleeping for %s seconds before proceeding' %
+                       self.sleep_time)
+        time.sleep(self.sleep_time)
+
+        # Check at HMC
+        r_msg = 'Removing %d megabytes of memory from partition %s.' % \
+                (quantity, linux_machine.partition)
+        r_condition = (int(self.get_mem_option(linux_machine, 'curr_mem')) == \
+                       (curr_mem_before - quantity))
+        if not self.log.check_log(r_msg, r_condition, False):
+            e_msg = 'Error happened when removing memory from %s' % \
+                    linux_machine.partition
+            self.log.error(e_msg)
+            raise TestException(e_msg)
+
+        # Check at Linux partition (using /proc/meminfo)
+        curr_mem_proc_after = self.__check_memory_proc(linux_machine)
+        p_msg = 'Checking if /proc/meminfo shows all removed memory.'
+        p_condition = ((curr_mem_proc_before - curr_mem_proc_after)/1024 == \
+                       quantity)
+        self.log.check_log(p_msg, p_condition)
+
+
+    def __move_memory(self, linux_machine_1, linux_machine_2, quantity):
+        """
+        Move 'quantity' of memory from linux partition 1 to linux partition 2.
+        """
+        # Get all values before adding
+        curr_mem_before_1 = int(self.get_mem_option(linux_machine_1,
+                                'curr_mem'))
+        curr_mem_before_2 = int(self.get_mem_option(linux_machine_2,
+                                'curr_mem'))
+
+        curr_mem_proc_before_1 = self.__check_memory_proc(linux_machine_1)
+        curr_mem_proc_before_2 = self.__check_memory_proc(linux_machine_2)
+
+        # Move the memory
+        m_cmd = 'chhwres -m ' + linux_machine_1.machine + ' -r mem -o m -q ' + \
+                str(quantity) + ' -p "' + linux_machine_1.partition + \
+                ' -t "' + linux_machine_2.partition + '"'
+        self.hmc.sshcnx.run_command(m_cmd)
+        self.log.debug('Going to sleep for %s s' % self.sleep_time)
+        time.sleep(self.sleep_time)
+
+        # Check at HMC
+        m_msg = 'Moving %d megabytes of memory from %s to %s.' % \
+                (quantity, linux_machine_1.partition, linux_machine_2.partition)
+        if not self.log.check_log(m_msg, 
+                                  (int(self.get_mem_option(linux_machine_1, 
+                                                           'curr_mem')) == \
+                                   curr_mem_before_1 - quantity) and \
+                                   (int(self.get_mem_option(linux_machine_2, 
+                                                            'curr_mem')) == \
+                                   curr_mem_before_2 + quantity), False):
+            e_msg = 'Error happened when moving memory from %s to %s' % \
+                    (linux_machine_1.partition, linux_machine_2.partition)
+            self.log.error(e_msg)
+            raise TestException(e_msg)
+
+        # Check at Linux partition (using /proc/meminfo)
+        curr_mem_proc_after_1 = self.__check_memory_proc(linux_machine_1)
+        curr_mem_proc_after_2 = self.__check_memory_proc(linux_machine_2)
+        c_msg_1 = 'Checking if /proc/meminfo shows removed memory on %s.' % \
+                  linux_machine_1.partition
+        self.log.check_log(c_msg_1, ((curr_mem_proc_after_1 - \
+                                      curr_mem_proc_before_1)/1024 == quantity))
+
+        c_msg_2 = 'Checking if /proc/meminfo shows removed memory on %s.' % \
+                  linux_machine_2.partition
+        self.log.check_log(c_msg_2, ((curr_mem_proc_before_2 - \
+                                      curr_mem_proc_after_2)/1024 == quantity))
+
+
+if __name__ == "__main__":
+    MEMORY = Memory()
+    MEMORY.run_test()
