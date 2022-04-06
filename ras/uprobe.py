c565a8f1730ec616207276e47166e7dfd05c9a8e
Samir Mulani <samir@linux.vnet.ibm.com>
2022-04-06 07:56:50 -0400

    Code optimization for uprobe.py test script.
    Code optimization for uprobe.py,
    The enable_uprobes and disable_uprobes functions have been removed and a single function has been added to control uprobes.
    Added support for handling system exceptions while writing data into the /sys/kernel/debug/tracing/ uprobe_events and other files.
    Added docstrings for methods.
    
    Signed-off-by: Samir Mulani <samir@linux.vnet.ibm.com>

--- ras/uprobe.py
+++ ras/uprobe.py
@@ -27,7 +27,6 @@ class Uprobe(Test):
     Test kernel uprobes
     :avocado: tags=privileged
     """
-
     def run_cmd(self, cmd):
         self.log.info("executing ============== %s =================", cmd)
         if process.system(cmd, ignore_status=True, sudo=True, shell=True):
@@ -36,27 +35,54 @@ class Uprobe(Test):
 
     @staticmethod
     def run_cmd_out(cmd):
-        return process.system_output(cmd, shell=True, ignore_status=True,
+        """
+        Run the linux terminal command
+        Ex: cat, ldd ..etc.
+        """
+        return process.system_output(cmd,
+                                     shell=True, ignore_status=True,
                                      sudo=True).decode("utf-8")
 
     def clear_trace(self):
-        self.debugfs = "/sys/kernel/debug/tracing/"
-        self.tracefs = os.path.join(self.debugfs, "trace")
-        self.uprobes_enable_fs = os.path.join(self.debugfs, "events/uprobes/enable")
-        self.uprobes_event_fs = os.path.join(self.debugfs, "events/uprobes/u_malloc/enable")
-        self.uprobes_events_fs = os.path.join(self.debugfs, "uprobe_events")
-        genio.write_one_line(self.tracefs, "")
-        genio.write_one_line(self.uprobes_events_fs, "")
+        """
+        Clear the tracepoint and uprobe events
+        before enable and disable the probe.
+        """
+        try:
+            self.debugfs = "/sys/kernel/debug/tracing/"
+            self.tracefs = os.path.join(self.debugfs, "trace")
+            uprobes_en = "events/uprobes/enable"
+            self.uprobes_enable_fs = os.path.join(self.debugfs, uprobes_en)
+            uprobes_umall = "events/uprobes/u_malloc/enable"
+            self.uprobes_event_fs = os.path.join(self.debugfs, uprobes_umall)
+            uprobe_eve = "uprobe_events"
+            self.uprobes_events_fs = os.path.join(self.debugfs, uprobe_eve)
+            genio.write_one_line(self.tracefs, "")
+            genio.write_one_line(self.uprobes_events_fs, "")
+        except IOError:
+            self.cancel("There is issue with kernel system resources")
 
-    def enable_uprobes(self):
-        genio.write_one_line(self.uprobes_enable_fs, "1")
-        genio.write_one_line(self.uprobes_event_fs, "1")
+    def ena_dis_uprobes(self, prob_value):
+        """
+        Enable and Disable the uprobes.
+        Enable_uprobes:
+        prob_value = 1
 
-    def disable_uprobes(self):
-        genio.write_one_line(self.uprobes_event_fs, "0")
-        genio.write_one_line(self.uprobes_enable_fs, "0")
+        Disable_uprobes:
+        prob_value = 0
+        """
+        try:
+            genio.write_one_line(self.uprobes_enable_fs, prob_value)
+            genio.write_one_line(self.uprobes_event_fs, prob_value)
+        except IOError:
+            self.cancel("There is issue with kernel system resources")
 
     def setUp(self):
+        """
+        Setup the system for test.
+        """
+        self.enable_prob = "1"
+        self.disable_prob = "0"
         dist = distro.detect()
         smg = SoftwareManager()
         if dist.name in ["Ubuntu", "unknown", 'debian']:
@@ -70,23 +96,31 @@ class Uprobe(Test):
                 self.cancel('%s is needed for the test to be run' % package)
 
     def execute_test(self):
+        """
+        -> This function is responsible to insert tracepoints in the kernel.
+        -> It will enable and disable the uprobes.
+        """
         self.log.info("============== Testing uprobes =================")
         self.is_fail = 0
         self.clear_trace()
-
-        libc_path = self.run_cmd_out("ldd /bin/bash|grep -i libc").split(" ")[2]
-        libc_addr = self.run_cmd_out("objdump -T %s | grep -w malloc" % libc_path).split(" ")[0]
-        uprobes_cmd = "echo 'p:u_malloc %s:0x%s' > %s" % (libc_path, libc_addr, self.uprobes_events_fs)
+        libcpath_cmd = "ldd /bin/bash|grep -i libc"
+        libc_path = self.run_cmd_out(libcpath_cmd).split(" ")[2]
+        libcaddr_cmd = "objdump -T %s | grep -w malloc"
+        libc_addr = self.run_cmd_out(libcaddr_cmd % libc_path).split(" ")[0]
+        uprobes = "echo 'p:u_malloc %s:0x%s' > %s"
+        uprobes_cmd = uprobes % (libc_path, libc_addr, self.uprobes_events_fs)
         self.run_cmd(uprobes_cmd)
         if self.is_fail:
             self.fail("Cannot plant a uprobes with %s", uprobes_cmd)
 
-        self.enable_uprobes()
-        self.run_cmd("date")
-        self.run_cmd("ls")
-        if "u_malloc" not in self.run_cmd_out("cat /sys/kernel/debug/tracing/trace"):
+        self.ena_dis_uprobes(self.enable_prob)
+        cmd_list = ["date", "ls"]
+        for cmd in cmd_list:
+            self.run_cmd(cmd)
+        cmd_trace = "cat /sys/kernel/debug/tracing/trace"
+        if "u_malloc" not in (self.run_cmd_out(cmd_trace)):
             self.fail("Uprobe probe was not hit.")
-        self.disable_uprobes()
+        self.ena_dis_uprobes(self.disable_prob)
         self.clear_trace()
 
     def test(self):
