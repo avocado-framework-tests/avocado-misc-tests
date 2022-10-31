3d85cc6fad1ae9dced10a2b55c06a53357cf2358
Shaik Abdulla <Shaik.Abdulla1@ibm.com>
2022-10-31 18:36:44 +0530

    NUMA support for Network and PCI device
    Patch for newly added test cases for NUMA network and PCI devices.
    
    Signed-off-by: Shaik Abdulla <Shaik.Abdulla1@ibm.com>

--- cpu/numactl.py
+++ cpu/numactl.py
@@ -17,10 +17,17 @@
 
 
 import os
+import glob
+import re
+import netifaces
+from random import choice
 
 from avocado import Test
 from avocado.utils import archive, build, process, distro, memory
 from avocado.utils.software_manager.manager import SoftwareManager
+from avocado.utils.network.interfaces import NetworkInterface
+from avocado.utils.network.hosts import LocalHost
+from avocado.utils import genio, process
 
 
 class Numactl(Test):
@@ -67,6 +74,28 @@ class Numactl(Test):
 
         build.make(self.sourcedir)
 
+        interfaces = netifaces.interfaces()
+        interface = self.params.get("interface")
+        if interface not in interfaces:
+            self.cancel("%s interface is not available" % interface)
+        self.iface = interface
+        local = LocalHost()
+        self.networkinterface = NetworkInterface(self.iface, local)
+        self.ipaddr = self.params.get("host_ip", default="")
+        self.netmask = self.params.get("netmask", default="")
+        self.peer = self.params.get("peer_ip")
+        if not self.peer:
+            self.cancel("No peer provided")
+        if self.networkinterface.ping_check(self.peer, count=5) is not None:
+            self.cancel("No connection to peer")
+        self.device = self.params.get('pci_device', default="")
+        if not self.device:
+            self.cancel("PCI_address not given")
+        if not os.path.isdir('/sys/bus/pci/devices/%s' % self.device):
+            self.cancel("%s not present in device path" % self.device)
+
+        self.ping_count = 5000000
+
     def test(self):
 
         if build.make(self.sourcedir, extra_args='-k -j 1'
@@ -75,3 +104,106 @@ class Numactl(Test):
                 self.log.warn('Few tests failed due to less NUMA mem-nodes')
             else:
                 self.fail('test failed, Please check debug log')
+
+    def get_nodes_with_cpus(self):
+        '''
+        Returns a dictonary, 
+        1. node number as key and 
+        2. associated CPUS as values. 
+        '''
+
+        cpu_path = '/sys/devices/system/node/has_cpu'
+        if not os.path.exists(cpu_path):
+            raise CpuError("No NUMA nodes have cpu")
+            self.cancel("No NUMA nodes have CPU")
+               
+        if len(genio.read_file(cpu_path).rstrip('\n')) < 2:
+            self.cancel("Required atleast two NUMA nodes with CPU assigned  for this test case!")
+
+        self.dict1 = {}
+        for path in glob.glob("/sys/devices/system/node/node[0-9]*"):
+            self.node= (int(re.search(r".node(\d+)$",path).group(1)))
+            self.node_dir = (path + "/")
+            self.pinned_cpus = []
+            for cpu_numbers in glob.glob(self.node_dir+"cpu[0-9]*"):
+                self.cpu = (int(re.search(r".cpu(\d+)$",cpu_numbers).group(1)))
+                self.pinned_cpus.append(self.cpu)
+            self.dict1[self.node] = (sorted(self.pinned_cpus))
+               
+        return (self.dict1)
+
+
+    def numa_ping(self,cmd):
+        '''
+        Ping Test to remote Peer with -f and count options.
+        '''
+        output = process.run(cmd, shell=True,ignore_status=True).stdout.decode("utf-8").split(",")
+        if " 0% packet loss"  not in output:
+            self.cancel("failed due to packet loss")
+    
+    def test_interleave(self):
+        '''
+        To check memory interleave on NUMA nodes.
+        '''
+                 
+        cmd = "numactl --interleave=all ping -I %s %s -c %s -f" % (self.iface, self.ipaddr, self.ping_count)
+        self.numa_ping(cmd)
+
+    
+    def test_localalloc(self):
+        '''
+        Test memory allocation on the current node
+        '''
+                 
+        cmd = "numactl --localalloc ping -I %s %s -c %s -f" % (self.iface, self.ipaddr, self.ping_count)
+        self.numa_ping(cmd)
+
+
+    def test_preferred_node(self):
+        '''
+        Test Preferably allocate memory on node
+        '''
+        self.get_nodes_with_cpus()
+        self.node_number = [key for key in self.dict1.keys()][1]
+        cmd = "numactl --preferred=%s  ping -I %s %s -c %s -f" % (self.node_number, self.iface, self.ipaddr, self.ping_count)
+        self.numa_ping(cmd)
+
+
+    def test_cpunode_with_membind(self):
+        '''
+        Test CPU and memory bind
+        '''
+        self.get_nodes_with_cpus()
+        self.first_cpu_node_number = [key for key in self.dict1.keys()][0]
+        self.second_cpu_node_number = [key for key in self.dict1.keys()][1]
+        self.membind_node_number = [key for key in self.dict1.keys()][1]
+        cmd = "numactl --cpunodebind=%s --membind=%s   ping -I %s %s -c %s -f " % (self.first_cpu_node_number,self.membind_node_number, self.iface, self.ipaddr, self.ping_count)
+        self.numa_ping(cmd)
+        cmd = "numactl --cpunodebind=%s --membind=%s   ping -I %s %s -c %s -f " % (self.second_cpu_node_number,self.membind_node_number, self.iface, self.ipaddr, self.ping_count)
+        self.numa_ping(cmd)
+
+
+    def test_physical_cpu_bind(self):
+        '''
+        Test physcial  CPU binds
+        '''
+        self.get_nodes_with_cpus()
+        self.cpu_number = [value for value in self.dict1.values()][0][1]
+        cmd = "numactl --physcpubind=%s ping -I %s %s -c %s -f" % (self.cpu_number, self.iface, self.peer, self.ping_count)
+        self.numa_ping(cmd)
+        
+
+    def test_numa_pci_bind(self):
+        '''
+        Test PCI binding to diferrent NUMA nodes 
+        '''
+        self.get_nodes_with_cpus()
+        node_numbers = [node for node in self.dict1.keys()]
+        pci_node_number = process.system_output('cat /sys/bus/pci/devices/%s/numa_node' %self.device)
+        node_path = ('/sys/bus/pci/devices/%s/numa_node' %self.device)
+        alternate_node = (choice([i for i in node_numbers if i not in [pci_node_number]]))
+        output = process.run('echo %s > %s' %(alternate_node,node_path),shell=True, ignore_status=True)
+        if output.exit_status != 0:
+            self.fail("Failed to assign the NUMA nodes to PCI, please check the logs")
+        else:
+            self.log.info(f"Assigned PCI from numa node {pci_node_number} to {alternate_node} sucessfully")
