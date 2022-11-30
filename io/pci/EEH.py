8119f21ec3a9851362cb5d66b41f021628c2cc6e
Maram Srimannarayana Murthy <Maram.Srimannarayana.Murthy@ibm.com>
2022-11-30 11:08:48 +0530

    EEH for block devies are updated. Added validation to check multipath devices after EEH.
    EEH for block devies are updated. Added validation to check multipath devices
    after EEH.
    
    Signed-off-by: Maram Srimannarayana Murthy <Maram.Srimannarayana.Murthy@ibm.com>

--- io/pci/EEH.py
+++ io/pci/EEH.py
@@ -26,6 +26,8 @@ from avocado.utils import pci
 from avocado.utils import genio
 from avocado.utils import distro
 from avocado.utils import dmesg
+from avocado.utils import multipath
+from avocado.utils import genericutility
 from avocado.utils.software_manager.manager import SoftwareManager
 
 EEH_HIT = 0
@@ -44,7 +46,9 @@ class EEHRecoveryFailed(Exception):
         self.log = log
 
     def __str__(self):
-        return "%s %s recovery failed: %s" % (self.msg, self.dev, self.log)
+        return f"{self.msg} {self.dev} recovery failed: {self.log}" % (
+                self.msg, self.dev, self.log
+            )
 
 
 class EEH(Test):
@@ -75,15 +79,15 @@ class EEH(Test):
         self.mem_addr = pci.get_memory_address(self.pci_device)
         self.mask = pci.get_mask(self.pci_device)
         if self.is_baremetal():
-            cmd = "echo %d > /sys/kernel/debug/powerpc/eeh_max_freezes"\
-                % self.max_freeze
+            cmd = f"echo {self.max_freeze} > /sys/kernel/debug/powerpc/eeh_max_freezes"
             process.system(cmd, ignore_status=True, shell=True)
             self.phb = self.pci_device.split(":", 1)[0]
-            self.addr = genio.read_file("/sys/bus/pci/devices/%s/"
-                                        "eeh_pe_config_addr" % self.pci_device)
+            self.addr = genio.read_file(
+                    f"/sys/bus/pci/devices/{self.pci_device}/eeh_pe_config_addr"
+            )
             self.addr = str(self.addr).rstrip()
             self.err = 0
-            for line in process.system_output('lspci -vs %s' % self.pci_device,
+            for line in process.system_output(f"lspci -vs {self.pci_device}",
                                               ignore_status=True,
                                               shell=True).decode("utf-8\
                                               ").splitlines():
@@ -105,10 +109,12 @@ class EEH(Test):
         enter_loop = True
         num_of_miss = 0
         num_of_hit = 0
-        while num_of_hit <= self.max_freeze:
+        while num_of_hit < self.max_freeze:
             for func in self.function:
                 self.log.info("Running error inject on pe %s function %s",
                               self.pci_device, func)
+                if pci.get_pci_class_name(self.pci_device) == "fc_host":
+                    before_eeh_path_status = multipath.get_multipath_details()
                 if num_of_miss < 5:
                     if self.is_baremetal():
                         return_code = self.basic_eeh(func, '', '', '', '', '')
@@ -121,31 +127,44 @@ class EEH(Test):
                                                      self.add_cmd)
                     if return_code == EEH_MISS:
                         num_of_miss += 1
-                        self.log.info("number of miss is %d", num_of_miss)
+                        self.log.info(f"number of miss is {num_of_miss}")
                         continue
                     else:
                         num_of_hit += 1
                         self.log.info("number of hit is %d", num_of_hit)
                         if num_of_hit <= self.max_freeze:
                             if not self.check_eeh_pe_recovery():
-                                self.fail("PE %s recovery failed after"
-                                          "%d EEH" % (self.pci_device,
-                                                      num_of_hit))
+                                self.fail(
+                                        f"PE {self.pci_device} recovery failed after {num_of_hit}"
+                                        f" EEH"
+                                )
                                 break
                             else:
                                 self.log.info("PE recovered successfully")
+                        time.sleep(10)
+                        if pci.get_pci_class_name(self.pci_device) == "fc_host":
+                            after_eeh_path_status = multipath.get_multipath_details()
+                            get_diff_bef_aft = genericutility.recursive_compare_dict(
+                                    before_eeh_path_status, after_eeh_path_status
+                                )
+                            if len(get_diff_bef_aft) != 0:
+                                for value in get_diff_bef_aft[:]:
+                                    if ("path_faults " in value) or ("switch_grp " in value):
+                                        get_diff_bef_aft.remove(value)
+                                if len(get_diff_bef_aft) != 0:
+                                    self.fail("Some devices/disks are failed to recover after EEH")
+                                    self.log.info(get_diff_bef_aft)
                 else:
-                    self.log.warning("EEH inject failed for 5 times with\
-                               function %s" % func)
+                    self.log.warning(f"EEH inject failed for 5 times with function {func}")
                     enter_loop = False
                     break
             if not enter_loop:
                 break
         else:
             if self.check_eeh_removed():
-                self.log.info("PE %s removed successfully", self.pci_device)
+                self.log.info(f"PE {self.pci_device} removed successfully")
             else:
-                self.fail("PE %s not removed after max hit" % self.pci_device)
+                self.fail(f"PE {self.pci_device} not removed after max hit")
 
     def basic_eeh(self, func, pci_class_name, pci_interface,
                   pci_mem_addr, pci_mask, add_cmd):
@@ -165,10 +184,10 @@ class EEH(Test):
         if return_code != EEH_HIT:
             self.log.info("Skipping verification, as command failed")
         if not self.check_eeh_hit():
-            self.log.info("PE %s EEH hit failed" % self.pci_device)
+            self.log.info(f"PE {self.pci_device} EEH hit failed")
             return EEH_MISS
         else:
-            self.log.info("PE %s EEH hit success" % self.pci_device)
+            self.log.info(f"PE {self.pci_device} EEH hit success")
             return EEH_HIT
 
     def error_inject(self, func, pci_class_name, pci_interface, pci_mem_addr,
@@ -177,13 +196,13 @@ class EEH(Test):
         Form a command to inject the error
         """
         if self.is_baremetal():
-            cmd = "echo %s:%s:%s:%s:%s > /sys/kernel/debug/powerpc/PCI%s/err_injct \
-                   && lspci; echo $?" % (addr, err, func, pci_mem_addr, pci_mask, phb)
+            cmd = f"echo {addr}:{err}:{func}:{pci_mem_addr}:{pci_mask} > "
+            cmd += f"/sys/kernel/debug/powerpc/PCI{phb}/err_injct && lspci; echo $?"
             return int(process.system_output(cmd, ignore_status=True,
                                              shell=True).decode("utf-8")[-1])
         else:
-            cmd = "errinjct eeh -v -f %s -s %s/%s -a %s -m %s; echo $?"\
-                % (func, pci_class_name, pci_interface, pci_mem_addr, pci_mask)
+            cmd = f"errinjct eeh -v -f {func} -s {pci_class_name}/{pci_interface}"
+            cmd += f" -a {pci_mem_addr} -m {pci_mask}; echo $?"
             res = process.system_output(cmd, ignore_status=True,
                                         shell=True).decode("utf-8")
             if add_cmd:
@@ -198,11 +217,8 @@ class EEH(Test):
         tries = 60
         for _ in range(0, tries):
             res = process.run(cmd, ignore_status=True, shell=True)
-            if 'EEH: Notify device driver to resume' in res.stdout.decode("utf-8") and \
-               res.exit_status != 0:
+            if 'EEH: Recovery successful.' in res.stdout.decode("utf-8"):
                 self.log.info("waiting for PE to recover %s", self.pci_device)
-                time.sleep(1)
-            else:
                 # EEH Recovery is not similar for all adapters. For some
                 # adapters, specifically multipath, we see that the adapter
                 # needs some more time to recover after the message "Notify
@@ -212,6 +228,7 @@ class EEH(Test):
                 # time of 10s is introduced.
                 time.sleep(10)
                 break
+            time.sleep(1)
         else:
             raise EEHRecoveryFailed("EEH recovery failed", self.pci_device)
         tries = 30
