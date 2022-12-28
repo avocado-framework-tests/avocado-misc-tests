3593a783efb202f912d42581cc53aa5e5c6c50c1
Sachin Sant <sachinp@linux.ibm.com>
2022-12-28 11:13:47 +0530

    trace/dawr: Fix compile time failure
    dawr test fails to compile the c code due to invalid
    preprocessing directive.
    
    Use correct style for comments in the c source code.
    
    Signed-off-by: Sachin Sant <sachinp@linux.ibm.com>

--- trace/dawr.py.data/dawr_v3.c
+++ trace/dawr.py.data/dawr_v3.c
@@ -1,16 +1,18 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# See LICENSE for more details.
-#
-# Copyright: 2022 IBM
-# Author: Akanksha J N <akanksha@linux.ibm.com>
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See LICENSE for more details.
+ *
+ * Copyright: 2022 IBM
+ * Author: Akanksha J N <akanksha@linux.ibm.com>
+*/
 
 #include<stdio.h>
 int a=10;
