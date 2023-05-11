f717410eb56944cfd478ef9db1a0fe87bd117a5c
Geetika <geetika@linux.ibm.com>
2023-05-11 10:01:06 +0530

    Improve the smt workload sh script
    1. ppc64_cpu --smt=8 and ppc64_cpu --smt=on mean the same thing.
    So there is no point running these commans one after another.
    
    2. added ppc64_cpu --smt=on followed by ppc64_cpu --smt=off so that
    we have an instance where maximum no. of threads in a core (7) go offline at once.
    
    Signed-off-by: Geetika <geetika@linux.ibm.com>

--- workload/smt.py.data/smt.sh
+++ workload/smt.py.data/smt.sh
@@ -19,10 +19,6 @@ do
 	sleep 2
 	ppc64_cpu --smt
 	time ppc64_cpu --smt=8
-	ppc64_cpu --info
-	sleep 2
-	ppc64_cpu --smt
-        time ppc64_cpu --smt=on
         ppc64_cpu --info
 	sleep 2
 	ppc64_cpu --smt
@@ -30,8 +26,11 @@ do
 	ppc64_cpu --info
 	sleep 2
 	ppc64_cpu --smt
-	time ppc64_cpu --smt=8
+	time ppc64_cpu --smt=on
 	ppc64_cpu --info
 	sleep 2
 	ppc64_cpu --smt
+	time ppc64_cpu --smt=off
+	ppc64_cpu --info
+	sleep 2
 done
