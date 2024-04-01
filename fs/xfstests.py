avocado-framework-tests/avocado-misc-tests

commit 3fc8e18d30aa939eccbec6021b3a1bfdafa7414a

fs/xfstests: Update package dependency
On SUSE some of the packages used by xfstest script are available
with a different name.

Update the __setUp_packages function accordingly to use the correct
package names.

Signed-off-by: Sachin Sant <sachinp@linux.ibm.com>

fs/xfstests.py
@@ -177,20 +177,21 @@ class Xfstests(Test):
             packages.extend(['acl', 'bc', 'indent', 'libtool', 'lvm2',
                              'xfsdump', 'psmisc', 'sed', 'libacl-devel',
                              'libattr-devel', 'libaio-devel', 'libuuid-devel',
-                             'openssl-devel', 'xfsprogs-devel', 'gettext',
                              'libblkid-devel', 'lzo-devel', 'zlib-devel',
                              'e2fsprogs-devel', 'asciidoc', 'xmlto',
                              'libzstd-devel', 'systemd-devel', 'meson',
-                             'gcc-c++'])
+                             'xfsprogs-devel', 'gcc-c++'])
             if self.detected_distro.name == 'rhel' and (
                     self.detected_distro.version.startswith('9')):
                 packages.extend(['inih-devel'])
 
             if self.detected_distro.name == 'SuSE':
                 packages.extend(['libbtrfs-devel', 'libcap-progs',
-                                'liburcu-devel', 'libinih-devel'])
+                                'liburcu-devel', 'libinih-devel',
+                                 'libopenssl-devel', 'gettext-tools'])
             else:
-                packages.extend(['btrfs-progs-devel', 'userspace-rcu-devel'])
+                packages.extend(['btrfs-progs-devel', 'userspace-rcu-devel'
+                                 'openssl-devel', 'gettext'])
 
             packages_remove = ['indent', 'btrfs-progs-devel']
             if self.detected_distro.name == 'rhel' and (

commit eb5a7fa6003b5eac21c915ab698ac6b95c95b508

fs/xfstest: Fix a regression introduced by 3fc8e18
Commit 3fc8e18 introduced a regression as it missed a comma
after userspace-rcu-devel.

Update the code accordingly to fix this regression.

Signed-off-by: Sachin Sant <sachinp@linux.ibm.com>

fs/xfstests.py
@@ -190,7 +190,7 @@ class Xfstests(Test):
                                 'liburcu-devel', 'libinih-devel',
                                  'libopenssl-devel', 'gettext-tools'])
             else:
-                packages.extend(['btrfs-progs-devel', 'userspace-rcu-devel'
+                packages.extend(['btrfs-progs-devel', 'userspace-rcu-devel',
                                  'openssl-devel', 'gettext'])
 
             packages_remove = ['indent', 'btrfs-progs-devel']