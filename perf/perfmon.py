avocado-framework-tests/avocado-misc-tests

commit 2ea45f72e0d06d0e58f3a9e3c1414102512b712c

Moved pmqa url from git to https
Moved pmqa url from git to https

Signed-off-by: Praveen K Pandey <praveen@linux.vnet.ibm.com>

perf/perfmon.py
@@ -45,7 +45,7 @@ class Perfmon(Test):
                 self.cancel(
                     "Fail to install %s required for this test." % package)
 
-        git.get_repo('git://git.code.sf.net/p/perfmon2/libpfm4',
+        git.get_repo('https://git.code.sf.net/p/perfmon2/libpfm4 perfmon2-libpfm4',
                      destination_dir=self.workdir)
 
         os.chdir(self.workdir)

cpu/pmqa.py
@@ -49,7 +49,7 @@ class Pmqa(Test):
                 self.cancel(
                     "Fail to install %s required for this test." % package)
 
-        git.get_repo('git://git.linaro.org/power/pm-qa.git',
+        git.get_repo('https://git.linaro.org/power/pm-qa.git',
                      destination_dir=self.workdir)
         self.test_type = self.params.get('run_arg', default='cpufreq')
 

commit 63244a8fab8a7f48ed57cc96c1fec54377d8b994

Update perfmon git url
Update perfmon git url
"git://git.code.sf.net/p/perfmon2/libpfm4"

Fixes: 2ea45f72e0d0 ("Moved pmqa url from git to https")

-------Before-------
 $ avocado run perf/perfmon.py
	JOB ID     : 7be44ae6aa9e0af0731416ddd9153da1516ce2a9
	JOB LOG    : tests/results/job-2024-06-10T06.33-7be44ae/job.log
	 (1/1) perfmon.py:Perfmon.test: STARTED
	 (1/1) perfmon.py:Perfmon.test: ERROR: Command '/bin/git fetch -q -f
		 -u -t https://git.code.sf.net/p/perfmon2/libpfm4
		perfmon2-libpfm4 master:master' failed.\nstdout:
		b''\nstderr: b"fatal: couldn't find remote ref
		perfmon2-libpfm4\n"\nadditional_info: None (0.27 s)
	RESULTS    : PASS 0 | ERROR 1 | FAIL 0 | SKIP 0 | WARN 0 |
			 INTERRUPT 0 | CANCEL 0
	JOB TIME   : 51.31 s

-------After------
 $ avocado run perf/perfmon.py
	JOB ID     : 7a9e3d95e05bbdcb230d562df2dc752b03984bc9
	JOB LOG    : tests/results/job-2024-06-10T06.33-7a9e3d9/job.log
	 (1/1) perfmon.py:Perfmon.test: STARTED
	 (1/1) perfmon.py:Perfmon.test: PASS (12.70 s)
	RESULTS    : PASS 1 | ERROR 0 | FAIL 0 | SKIP 0 | WARN 0 |
			INTERRUPT 0 | CANCEL 0
	JOB TIME   : 17.40 s

Signed-off-by: Ayush Jain <ayush.jain3@amd.com>

perf/perfmon.py
@@ -45,7 +45,7 @@ class Perfmon(Test):
                 self.cancel(
                     "Fail to install %s required for this test." % package)
 
-        git.get_repo('https://git.code.sf.net/p/perfmon2/libpfm4 perfmon2-libpfm4',
+        git.get_repo('https://git.code.sf.net/p/perfmon2/libpfm4',
                      destination_dir=self.workdir)
 
         os.chdir(self.workdir)