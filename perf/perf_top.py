08dec46512dbb5269a7c0a49fa2960083606e47e
root <root@ltcfuji9aq-lp13.ltc.tadn.ibm.com>
2025-11-19 20:03:21 +0530

    perf_top.py:test_top$ Added fix to eliminate pexpect and pexpect:spawn issue
    The pexpect issue is fixed by using alternative as pexpect while running
    is not recognised.
    
    Signed-off-by: Tejas Manhas <Tejas.Manhas1@ibm.com>

--- perf/perf_top.py
+++ perf/perf_top.py
@@ -17,7 +17,7 @@
 import sys
 import time
 import platform
-import pexpect
+import threading
 import tempfile
 import os
 from avocado import Test
@@ -76,15 +76,31 @@ class perf_top(Test):
                 self.option = self.option + " /boot/vmlinux-" + \
                         platform.uname()[2]
 
-        child = pexpect.spawn("perf top %s" % self.option, encoding='utf-8')
-        time.sleep(10)
-        child.logfile = sys.stdout
-        err = child.expect_exact(
-            ['Error: ', 'perf: Segmentation fault', pexpect.TIMEOUT])
-        child.send('q')
-        exit_status = child.wait()
-        if exit_status != 0 or err == 0:
-            self.fail("Unknown option %s" % self.option)
+        cmd = f"perf top {self.option}"
+        self.log.info(f"Running command: {cmd}")
+
+        proc = process.SubProcess(cmd)
+        proc.start()
+        time.sleep(10)  # let perf top run for a short while
+
+        # Try to stop gracefully (like pressing 'q')
+        if proc.poll() is None:
+            process.run(f"pkill -SIGINT -f '{cmd}'", ignore_status=True)
+            time.sleep(1)
+
+        proc.wait()
+
+        # Check for dmesg errors
+        dmesg.collect_errors_dmesg([
+            'WARNING: CPU:', 'Oops', 'Segfault',
+            'soft lockup', 'Unable to handle'
+        ])
+
+        # Check exit code and stderr for issues
+        if proc.result.exit_status != 0:
+            self.fail(f"perf top failed with option {self.option}: "
+                      f"{proc.result.stderr.decode('utf-8', 'ignore')}")
+
         dmesg.collect_errors_dmesg(['WARNING: CPU:', 'Oops', 'Segfault',
                                     'soft lockup', 'Unable to handle'])
 
