90b9a640e66dbdd9ef586863cd724097c8959e83
Priyanka Behera <priyankabehera@mac.bl1-in.ibm.com>
2026-05-19 15:13:37 +0530

    Update driver_parameter_block_device for ibmvscsi
    - Update driver_parameter_block_device.py to support ibmvscsi driver
    - Update driver_parameter_block_device_ibmvscsi.yaml configuration
    - Remove obsolete io/pci/dlpar.py.data/dlpar.yaml file

--- io/driver/driver_parameter_block_device.py
+++ io/driver/driver_parameter_block_device.py
@@ -1,9 +1,9 @@
-!/usr/bin/env python
+#!/usr/bin/env python
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
-# (at yo#ur option) any later version.
+# (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -108,8 +108,6 @@ class Moduleparameter(Test):
         '''
         process.system("multipath -F", ignore_status=True)
         cmd = "lsmod | grep -i ^%s" % self.module
-        # To-Do: more debug needed as mutlipath is restarting immediately
-        # after flush for qla2xxx only and we can remove the static sleep.
         time.sleep(10)
         if process.getoutput(cmd).split(" ")[-1] == '0':
             return True
