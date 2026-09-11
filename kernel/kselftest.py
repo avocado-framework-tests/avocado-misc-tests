avocado-framework-tests/avocado-misc-tests

commit c2809842bb14d72b8737ff48739070b3595a14bc

kselftest run from custom linux source
The upstream run type of kselftest currently requires a Git repository
URL and branch name to fetch the Linux kernel source. This commit
introduces a custom run type, allowing users to specify a local directory
containing the required Linux kernel source. The directory is validated
for existence, a Makefile, and the tools/testing/selftests directory to
ensure it is a valid kernel source from which kselftests can be run.

Signed-off-by: Srikanth Aithal <srikanth.aithal@amd.com>

kernel/kselftest.py
@@ -107,34 +107,49 @@ class kselftest(Test):
                 self.cancel(
                     "Fail to install %s package" % (package))
 
-        if self.run_type == 'upstream':
-            location = self.params.get('location', default='https://github.c'
-                                       'om/torvalds/linux/archive/master.zip')
-            if re.match(r'^https|^git', location):
-                git_branch = self.params.get('branch', default='master')
-                path = ''
-                match = next(
-                    (ext for ext in [".zip", ".tar", ".gz"] if ext in location), None)
-                if match:
-                    tarball = self.fetch_asset("kselftest%s" % match,
-                                               locations=[location], expire='1d')
-                    extracted_dir = archive.uncompress(tarball, self.workdir)
-                    path = glob.glob(os.path.join(self.workdir, extracted_dir))
-                else:
-                    git.get_repo(location, branch=git_branch,
-                                 destination_dir=self.workdir)
-                    path = glob.glob(self.workdir)
-                for l_dir in path:
-                    if os.path.isdir(l_dir) and 'Makefile' in os.listdir(l_dir):
-                        self.buldir = os.path.join(self.workdir, l_dir)
-                        break
-
-                self.sourcedir = os.path.join(self.buldir, self.testdir)
-                if (self.comp != "cpufreq" and self.comp != "bpf"):
-                    process.system("make headers -C %s" % self.buldir, shell=True,
-                                   sudo=True)
-                    process.system("make install -C %s" % self.sourcedir,
-                                   shell=True, sudo=True)
+        if self.run_type == 'custom' or self.run_type == 'upstream':
+            if self.run_type == 'custom':
+                linux_dir = self.params.get('linux_dir', default=None)
+                if not linux_dir or not os.path.exists(linux_dir):
+                    self.cancel(
+                        "Custom kernel source directory %s does not exist" % (linux_dir))
+                linux_dir = os.path.abspath(os.path.expanduser(linux_dir))
+                if not os.path.exists(os.path.join(linux_dir, "tools/testing/selftests")):
+                    self.cancel(
+                        "Custom kernel source directory %s is not a valid kernel source" % linux_dir)
+                if not os.path.exists(os.path.join(linux_dir, "Makefile")):
+                    self.cancel(
+                        "Custom kernel source directory %s lacks a Makefile" % linux_dir)
+                path = [linux_dir]
+            if self.run_type == 'upstream':
+                location = self.params.get('location', default='https://github.c'
+                                           'om/torvalds/linux/archive/master.zip')
+                if re.match(r'^https|^git', location):
+                    git_branch = self.params.get('branch', default='master')
+                    path = ''
+                    match = next(
+                        (ext for ext in [".zip", ".tar", ".gz"] if ext in location), None)
+                    if match:
+                        tarball = self.fetch_asset("kselftest%s" % match,
+                                                   locations=[location], expire='1d')
+                        extracted_dir = archive.uncompress(
+                            tarball, self.workdir)
+                        path = glob.glob(os.path.join(
+                            self.workdir, extracted_dir))
+                    else:
+                        git.get_repo(location, branch=git_branch,
+                                     destination_dir=self.workdir)
+                        path = glob.glob(self.workdir)
+            for l_dir in path:
+                if os.path.isdir(l_dir) and 'Makefile' in os.listdir(l_dir):
+                    self.buldir = os.path.join(self.workdir, l_dir)
+                    break
+            self.sourcedir = os.path.join(self.buldir, self.testdir)
+            if (self.comp != "cpufreq" and self.comp != "bpf"):
+                process.system("make headers -C %s" % self.buldir, shell=True,
+                               sudo=True)
+                process.system("make install -C %s" % self.sourcedir,
+                               shell=True, sudo=True)
             else:
                 self.buldir = self.params.get('location', default='')
         else:
@@ -201,7 +216,8 @@ class kselftest(Test):
                     test_comp = self.comp
                 make_cmd = 'make -C %s %s -C %s run_tests' % (
                     self.sourcedir, kself_args, test_comp)
-                self.result = process.run(make_cmd, shell=True, ignore_status=True)
+                self.result = process.run(
+                    make_cmd, shell=True, ignore_status=True)
         log_output = self.result.stdout.decode('utf-8')
         results_path = os.path.join(self.outputdir, 'raw_output')
         with open(results_path, 'w') as r_file:

commit 22ea29dd7b69fe1c7c3850e28d74b2eb89cf29fc

kernel/kselftest: Fix buldir path handling for custom run_type
Two bugs introduced by commit c2809842 (kselftest run from custom
linux source):

1. self.buldir was incorrectly set to os.path.join(self.workdir, l_dir)
   where l_dir is already an absolute path. Changed to self.buldir = l_dir.

2. The else block unconditionally overwrote self.buldir with the
   'location' param when comp == 'bpf' or 'cpufreq', wiping out the
   correctly set buldir for custom run_type. Guarded it with
   elif self.run_type == 'upstream' so custom run_type retains
   the correct buldir.

Signed-off-by: Venkat Rao Bagalkote <venkat@linux.ibm.com>

kernel/kselftest.py
@@ -152,7 +152,7 @@ class kselftest(Test):
                         path = glob.glob(self.workdir)
             for l_dir in path:
                 if os.path.isdir(l_dir) and 'Makefile' in os.listdir(l_dir):
-                    self.buldir = os.path.join(self.workdir, l_dir)
+                    self.buldir = l_dir
                     break
             self.sourcedir = os.path.join(self.buldir, self.testdir)
             if (self.comp != "cpufreq" and self.comp != "bpf"):
@@ -163,7 +163,7 @@ class kselftest(Test):
                 if not self.comp:
                     process.system("make install -C %s" % self.sourcedir,
                                    shell=True, sudo=True)
-            else:
+            elif self.run_type == 'upstream':
                 self.buldir = self.params.get('location', default='')
         else:
             # Make sure kernel source repo is configured